d8703e755744a086dac7069f8cf30cc9c2791c23
Emanuelle Frota <emanuellebc@gmail.com>
2017-10-26 18:18:58 -0300

    today

---
+++ amo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << " \t   *   "<<"\t *   \t   *" <<"   \t **** \t" <<endl;
+	cout << " \t  * *  "<< "\t * *     * *" << "\t *  *\t" << endl; 
+	cout << " \t ***** "<<"\t *  *  *   *" << "\t *  *\t" << endl; 
+	cout << " \t*     *"<<"\t *    *    *" <<"\t ****\t" << endl;   
+	
+	return 0;
+}
