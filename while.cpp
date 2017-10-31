73dc9643f15587ea5d12d3aa9a55aac8b74a7600
Emanuelle Frota <emanuellebc@gmail.com>
2017-10-31 19:51:40 -0300

    Um timido while

---
+++ while.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int n; 
+	
+	cout << "Digite um numero: " << endl;
+	cin >> n;
+	
+	while(n <=100)
+	{
+		if (n > 20 ) 
+	    break;
+	    cout << n << endl;
+	    n++; 
+	}	
+	return 0;
+}
