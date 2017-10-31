eda24c88bc834225ec59b75d4a74122ad59133f9
Emanuelle Frota <emanuellebc@gmail.com>
2017-10-30 21:11:49 -0300

    Exemplo de swith/case

---
+++ swithcase.cpp
@@ -0,0 +1,42 @@
+// if else (se senão)
+// switch case
+
+#include <iostream>
+#include <cstdlib>
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+	int x;
+	cout << "Digite um valor inteiro: " << endl;
+	cin >> x;
+	if(x==7)
+	{
+		cout << "Você digitou o número 7.";
+	}
+	else
+	{
+		cout << "Você não digitou o número 7.";
+	}
+	cout << endl;
+	system("pause");
+	
+	// Estrutura realizada com switch case
+	
+	cout << "-----------------------------" << endl << endl;
+	
+	switch(x)
+	{
+		case 7:
+			cout << "você digitou o 7.";
+			break;
+		default:
+			cout << "Você não digitou o número 7.";
+			break;
+	}
+	
+	cout << endl;
+	system("pause");
+}
