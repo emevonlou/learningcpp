4cad60d838016ef4b33d1d8a46e18641f7173637
Emanuelle Frota <emanuellebc@gmail.com>
2017-11-03 12:50:48 -0300

    Ifcomp

---
+++ ifcomp.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+ using namespace std;
+
+ int main(void){
+ int idade;
+ char sexo;
+
+ cout<<"Qual a sua idade?"<<endl;
+ cin>>idade;
+ cout<<"Digite (m)para Masculino, (f)para Feminino"<<endl;
+ cin>>sexo;
+
+ if (idade >= 18)
+ {
+ cout<<"Você pode dirigir"<<endl;
+
+ if (sexo=='m')
+ {
+
+cout<<"Você precisa ir no Serviço Militar"<<endl;
+ }
+ }
+ else 
+ {
+ if (idade > 16)
+ {
+ cout<<"Você já pode votar"<<endl;
+ }
+ else 
+ {
+   cout<<"Você não pode votar nem dirigir"<<endl;
+ }
+ }
+ 
+ return 0;
+ }
