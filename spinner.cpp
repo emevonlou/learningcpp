65b8db3d63efd99ba30f7e9d4455a258068b64c2
Emanuelle Costa <eme@vonlou.dev>
2017-11-04 19:05:50 -0300

    nhamy

---
+++ spinner.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "\\";
+    cout << "|";
+    cout << "-";
+    cout << "/";
+    cout << "|";
+    return 0;
+}
