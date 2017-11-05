345519d20bb5c74126c005376f980917a8032964
Emanuelle Costa <eme@vonlou.dev>
2017-11-04 21:15:11 -0300

    nhamy3

---
+++ comparing.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    char x1[20], x2[20];
+
+    cout << "Digite uma palavra: ";
+    cin.getline(x1,20);
+
+    cout << "Digite outra palavra: ";
+    cin.getline(x2,20);
+
+    if(x1 > x2)
+    {
+        cout << "\n" << x1 << "\t" << x2;
+    }
+    else
+    {
+        cout << "\n" << x2 << "\t" << x1;
+    }
+    cout << "\nx1:" << x1 << "\nx2: " << x2 << endl << endl;
+
+   system("pause");
+}
