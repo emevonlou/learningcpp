c9c7b4572599dc9ce37b06b42e54abd03ed8126e
Emanuelle Costa <eme@vonlou.dev>
2017-11-06 16:00:27 -0300

    Colors in library conio.h

---
+++ colors.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h> // comando system("color 109");
+
+using namespace std;
+
+int main()
+{
+    system("color 109");
+    printf("eme");
+}
