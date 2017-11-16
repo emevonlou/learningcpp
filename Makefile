d3336aa8e6b78325d15361426533146d77c977fb
Emanuelle Costa <eme@vonlou.dev>
2017-11-16 17:16:06 -0300

    Make simple Decisions!!Wow!

--- Makefile
+++ Makefile
@@ -42,4 +42,5 @@ build:
 	g++ -c -o varglob.o varglob.cpp; g++ -o varglob varglob.o
 	g++ -c -o modulo.o modulo.cpp; g++ -o modulo modulo.o
 	g++ -c -o operadores.o operadores.cpp; g++ -o operadores operadores.o
+	g++ -c -o decision.o decision.cpp; g++ -o decision decision.o
 .PHONY:
---
+++ decision.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1,n2;
+
+    cout << "Digite o primeiro numero: " << endl;
+    cin >> n1;
+    cout << "Digite o segundo numero: " << endl;
+    cin >> n2;
+
+    if(n1 > n2)
+    {
+        cout << "O primeiro numero eh maior que o segundo!!" << endl;
+    }
+    else
+    {
+        cout << "O primeiro numero eh menor que o segundo!!" << endl;
+    }
+    if(n1 == n2)
+    {
+        cout << "O primeiro numero eh igual ao segundo!!" << endl;
+    }
+    else
+    {
+        cout << "O primeiro numero eh diferente do segundo!" << endl;
+    }
+
+    return 0;
+}
