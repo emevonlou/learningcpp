ea03bc951fba2e750deeb307cf80d5dd112fa861
Emanuelle Frota <emanuellebc@gmail.com>
2017-11-16 16:27:53 -0300

    operadores unários

---
+++ operadores.cpp
@@ -0,0 +1,15 @@
+// operadores unários
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 20;
+    x += 2;
+    x++;
+
+    cout << " o resultado eh: " << x << endl;
+    
+
+ return 0;
+}
