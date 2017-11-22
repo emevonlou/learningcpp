7145c496be5f926aa81c5caa89feba1d2c52a4bd
Emanuelle Costa <eme@vonlou.dev>
2017-11-22 11:25:03 -0300

    Laço for

---
+++ para3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int conta, num=1, s=0;
+
+for(conta = 10; conta <= 40; conta= conta+10)
+{
+     s= s+num;
+     num= num +2;
+}
+
+cout << s << endl;
+cout << num << endl; 
+}
