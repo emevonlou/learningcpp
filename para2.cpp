8b61170f15eee8cea9af808a47aaee7694b2bf14
Emanuelle Frota <emanuellebc@gmail.com>
2017-11-22 11:12:17 -0300

    Laço for

---
+++ para2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    double x, y;
+    cout << "Digite um numero: ";
+    cin >> x;
+    for (i=0; i<5; i++){
+        cout << "Digite um numero: ";
+        cin >> y;
+        if (y > x){
+           x = y;      
+        }
+    }
+    cout << "Numero: " << x << "\n";
+    system("PAUSE");
+    return 0;
+}
