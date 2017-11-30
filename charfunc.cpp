967048bea4c2c789e4b4ec6603fe347d6ce50c5c
Emanuelle Costa <eme@vonlou.dev>
2017-11-30 15:48:38 -0300

    Playing with chargit add .

---
+++ charfunc.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+#include <ctype.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    char c = 'A';
+
+    if(isalpha(c))
+        cout << "Alfabetico" << endl;
+    else
+        cout << "Nao alfabetico" << endl;
+        
+    return 0;
+}
