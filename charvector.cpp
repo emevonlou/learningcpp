4b30a9a627837ab06ea9373366867ddb3df705bc
Emanuelle Frota <emanuellebc@gmail.com>
2017-11-30 15:26:53 -0300

    little char vector

--- charvector.cpp
+++ charvector.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    char nome[] = {'E','M','E','V','O','N','L','O','U', '\0'};
+    char nome[] = "Emanuelle Von Lou";
     
     for(int i = 0; nome[i] != '\0'; i++)
     {
