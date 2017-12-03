1142551b19dcc91175f6542a03a91cdfe1f795af
Emanuelle Costa <eme@vonlou.dev>
2017-12-03 20:56:42 -0300

    simple rand

--- rand2.cpp
+++ rand2.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime> 
 using namespace std;
 
 int main()
 {
+    srand(time(0)); // conta os segundos no meu computador e gera um valor
     cout << rand();  
 
     for(int i = 0; i < 20; i++)
