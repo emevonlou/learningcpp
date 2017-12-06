a74097ee40920b133c65b0bdfc294161bed05247
Emanuelle Frota <emanuellebc@gmail.com>
2017-12-06 10:03:06 -0300

    Matrizes

---
+++ matrizes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int mat[2][2] = {{1,2},{3,4}};
+    /* 
+        Dois colchetes(o primeiro das linhas, o segundo das colunas)
+        Matriz inicializada(primeira linha{1,2};segunda linha{3,4})
+    */
+    for(int i = 0; i < 2; i++)
+    {
+        for(int j = 0; j < 2; i++)
+        {
+            cout << mat[i][j] << "\t"; // O 'i' acessa linhas e o 'j' acessa colunas.
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
