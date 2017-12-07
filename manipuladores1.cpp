e4171d5d69ad3ea8e2c01c01c3881de6fd0a488c
Emanuelle Costa <eme@vonlou.dev>
2017-12-07 16:25:55 -0300

    Manipuladores!

--- manipuladores1.cpp
+++ manipuladores1.cpp
@@ -1,4 +1,4 @@
-#include <iostream> // traz um conjunto de instruções chamados de manipuladores.
+#include <iostream> // Traz um conjunto de instruções chamados de manipuladores.
 #include <stdlib.h> // system("pause")
 #include <iomanip> //getw()
 using namespace std;
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     //obj cin
     //obj cout
 
-    cout << "Estudando a entrada e saida de dados: " << endl;
+    cout << "Estudando a entrada e saida de dados: " << endl; //OBJETO DE SAÍDA(cout)
     cout << setw(1000) << 19 + 40 << endl;
 
     system("pause");
