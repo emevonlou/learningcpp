3e10a1d2bb80e1fd36c61a62a90d6619b2879f02
Emanuelle Costa <eme@vonlou.dev>
2017-12-07 16:34:27 -0300

    Manipuladores!

--- manipuladores.cpp
+++ manipuladores.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
+#include <iostream> //Tem implementada palavras especiais pra manipularmos a formatação.
 #include <stdlib.h>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    //obj cin
-    //obj cout
+    //obj cin = console.input
+    //obj cout = console.output
 
     cout << "Estudando a entrada e saida de dados: " << endl;
     cout << hex << 19 + 40 << endl;
