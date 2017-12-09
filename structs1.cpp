f2e1b32a810b16285903fc2a7ee4d80ba1eaa6b4
Emanuelle Costa <eme@vonlou.dev>
2017-12-09 16:16:59 -0300

    Structs.

---
+++ structs1.cpp
@@ -0,0 +1,26 @@
+//structs: Tem por objetivo agrupar os dados.
+#include <iostream>
+#include <string.h> // strcpy()
+using namespace std;
+
+typedef struct pessoa  //Definindo um nome para 'struct pessoa' com typedef
+{
+    char nome[100];
+    int idade;
+    float peso;
+}tipo_pessoa; // ao invés de digitar sempre 'struct pessoa', digita 'tipo_pessoa'
+
+int main(int argc, char *argv[])
+{
+    tipo_pessoa p;
+
+    strcpy(p.nome, "Emanuelle");
+    p.idade = 38;
+    p.peso = 44.3;
+
+    cout << "Nome: " << p.nome << endl;
+    cout << "Idade: " << p.idade << endl;
+    cout << "Peso: " << p.peso << endl;
+    
+    return 0;
+}
