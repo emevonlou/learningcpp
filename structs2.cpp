9f84c4f3003b40739c20d6b6b0275ac06efca01e
Emanuelle Costa <eme@vonlou.dev>
2017-12-09 17:19:06 -0300

    Structs.

---
+++ structs2.cpp
@@ -0,0 +1,29 @@
+//structs com ponteiros!
+#include <iostream>
+#include <string.h> // strcpy()
+using namespace std;
+
+typedef struct pessoa  //Definindo um nome para 'struct pessoa' com typedef
+{
+    char nome[100];
+    int idade;
+    float peso;
+}tipo_pessoa; 
+
+int main(int argc, char *argv[])
+{
+    tipo_pessoa p;
+    tipo_pessoa *pp;
+
+    pp = &p;
+
+    strcpy(pp->nome, "Emanuelle");
+    pp->idade = 38;
+    pp->peso = 44.3;
+
+    cout << "Nome: " << pp->nome << endl; // para acessar a struct com um ponteiro vc deve utilizar a seta '->'
+    cout << "Idade: " << pp->idade << endl;
+    cout << "Peso: " << pp->peso << endl;
+    
+    return 0;
+}
