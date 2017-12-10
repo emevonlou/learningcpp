bc4f27a0f1c90bcd924b0a59b8d157bf22616013
Emanuelle Costa <eme@vonlou.dev>
2017-12-10 13:22:12 -0300

    Make with Class!

--- classe.cpp
+++ classe.cpp
@@ -4,19 +4,42 @@ using namespace std;
 
 class Pessoa   // nome da classe com letra maiúscula
 {
-public:    // modificador de acesso
+private:    // modificador de acesso
     int idade;
     int peso;
+
+public:
+
+    void setIdade(int idade)
+    {
+        this->idade = idade;
+    }
+
+    int getIdade()
+    {
+        return idade;
+    }
+
+    void setPeso(float peso)
+    {
+        this->peso = peso;
+    }
+
+    float getPeso()
+    {
+        return peso;
+    }
+
 };
 
 int main(int argc, char *argv[])
 {
     Pessoa pessoa;
 
-    pessoa.idade = 28;  // Acesso aos membros do objeto
-    pessoa.peso = 75.56;
+    pessoa.setIdade(38);  // Acesso aos membros do objeto
+    pessoa.setPeso(44.3);
 
-    cout << "Idade: " << pessoa.idade << endl;
+    cout << "Idade: " << pessoa.getIdade() << endl;
 
  return 0;
 }
