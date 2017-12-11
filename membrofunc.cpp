ed875edd5acfe23cdf3ed9bad110894786dfda1f
Emanuelle Frota <emanuellebc@gmail.com>
2017-12-11 14:41:42 -0300

    Sobrecarregando Funções membro

---
+++ membrofunc.cpp
@@ -0,0 +1,28 @@
+// Sobrecarregando funções membro 
+#include <iostream>
+using namespace std;
+
+class Carro
+{
+public:
+
+	void mostrarMensagem()
+	{
+		cout << "Primeira Funcao" << endl;
+	}
+
+	void mostrarMensagem(int ano)
+	{
+		cout << "Ano: " << ano << endl;
+	}
+};
+
+int main(int argc, char *argv[])
+{
+	Carro c;
+
+	c.mostrarMensagem();
+	c.mostrarMensagem(2016);
+
+	return 0;
+}
