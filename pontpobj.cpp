5bb03b1e356ff793ebffcea3827d4b1fec4020cf
Emanuelle Frota <emanuellebc@gmail.com>
2017-12-11 15:20:19 -0300

    Ponteiros para objetos

---
+++ pontpobj.cpp
@@ -0,0 +1,29 @@
+// Ponteiros para Objetos!
+#include <iostream>
+using namespace std;
+
+class Carro
+{
+public:
+
+	void mostrarMensagem()
+	{
+		cout << "Primeira Funcao" << endl;
+	}
+
+	void mostrarMensagem(int ano)
+	{
+		cout << "Ano: " << ano << endl;
+	}
+};
+
+int main(int argc, char *argv[])
+{
+	Carro carro;
+	carro* pcarro;
+	
+	pcarro = &carro;
+
+	pcarro->mostrarMensagem();
+	return 0;
+}
