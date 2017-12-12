68e9d85b698035796add45eff05d95481233aba3
Emanuelle Frota <emanuellebc@gmail.com>
2017-12-12 16:34:06 -0300

    Sobrecarregando Construtores

---
+++ construct.cpp
@@ -0,0 +1,49 @@
+/*
+	Sobrecarregando o construtor
+	é possivel ter dois ou mais construtores.
+*/
+#include <iostream>
+using namespace std;
+
+class Estudante
+{
+private:
+	char nome[100];
+	int idade;
+public: 
+
+	Estudante(const char * nome)
+	{
+		strcpy(this->nome, "Desconhecido");
+		idade = -1;
+	}
+
+	Estudante(int idade)
+	{
+		this->idade = idade;
+	}
+
+	int getIdade()
+	{
+		return idade;
+	}
+
+	void mostrarPessoa()
+	{
+		cout << "Nome: " << nome << endl;
+		if (idade != -1)
+			cout << "Idade: " << idade << endl;
+		else
+			cout << "Idasde desconhecida." << endl;
+	}
+};
+
+int main(int argc, char *argv[])
+{
+	Estudante e1("Emanuelle"), e2(28);
+
+	e1.mostrarPessoa();
+	e2.mostrarPessoa();
+
+	return 0;
+}
