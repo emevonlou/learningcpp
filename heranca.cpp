b75858af5c3c02b906bd9c4a0a0f4d8fd755dfc7
Emanuelle Frota <emanuellebc@gmail.com>
2017-12-12 18:07:30 -0300

    Herança entre classes

---
+++ heranca.cpp
@@ -0,0 +1,57 @@
+/*
+	Herança!
+	-Transmite propriedades.
+	-Permite que vc descreva coisas de forma econômica.
+	-Uma classe herda características de outra classe.
+*/
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Mamifero
+{
+private:
+	float peso;
+	int idade;
+public:
+	Mamifero(float peso, int idade)
+	{
+		this->peso = peso;
+		this->idade = idade;
+	}
+	float getPeso()
+	{
+		return peso;
+	}
+	int getIdade()
+	{
+		return idade;
+	}
+
+};
+
+class Baleia: public Mamifero
+{
+private:
+	char oceano[50];
+public:
+	Baleia(const char * oceano, float peso, int idade):
+		Mamifero(peso,idade)
+	{
+		strcpy(this->oceano, oceano);
+	}
+	void mostrarBaleia()
+	{
+		cout << "Oceano: " << oceano << endl;
+		cout << "Peso: " << getPeso() << endl;
+		cout << "Idade: " << getIdade() << endl;
+	}
+};
+int main(int argc, char *argv[])
+{
+	Baleia baleia("Pacifico", 2000, 20);
+
+	baleia.mostrarBaleia();
+
+	return 0;
+}
