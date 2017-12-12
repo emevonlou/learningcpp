0658fc3373011ca7b95eade1e501a9311d46fb8c
Emanuelle Frota <emanuellebc@gmail.com>
2017-12-12 14:13:05 -0300

    Membros protegidos!

---
+++ protect.cpp
@@ -0,0 +1,29 @@
+/* 
+	Membros protegidos(Protected)
+	Inacessiveis de fora da classe.
+	permite que uma classe esteja protegida
+	e seja responsavel pela sua própria situação interna.
+*/
+#include <iostream>
+using namespace std;
+
+class Estudante
+{
+protected:      // Membros protegidos 
+	int idade;
+public:
+	void setIdade(int idade) //
+	{
+		this->idade = idade;
+		cout << "Idade: " << idade << endl;
+	}
+};
+
+int main(int argc, char *argv[])  // função externa
+{
+	Estudante e; // Objeto
+
+	e.setIdade(28);
+
+	return 0;
+}
