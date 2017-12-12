87cf5a1926bfdfaed590ce395529edcdff7410a1
Emanuelle Costa <eme@vonlou.dev>
2017-12-12 14:51:07 -0300

    Membros protegidos!

---
+++ friend.cpp
@@ -0,0 +1,29 @@
+/*
+	Funções amigas!
+*/
+#include <iostream>
+using namespace std;
+
+class Estudante
+{
+	friend void setIdade(Estudante * e, int idade);
+protected:      // Membros protegidos 
+	int idade;
+};
+
+void setIdade(Estudante * e, int idade)
+{
+	e->idade = idade;
+	cout << "Idade: " << e->idade << endl;
+}
+
+int main(int argc, char *argv[])
+{
+	Estudante * e; // Objeto
+
+	e = new Estudante;
+
+	setIdade(e,28);
+
+	return 0;
+}
