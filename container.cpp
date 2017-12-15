e103becdb56a322f897e05ebd9ce6aaecc8744ec
emevonlou <emanuellebc@gmail.com>
2017-12-15 13:52:42 -0300

    Container

---
+++ container.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector> //classe vector
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    vector<int> v;
+
+    v.push_back(10);
+    v.push_back(12);
+    v.push_back(20);
+    v.push_back(30);
+
+    for(int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << endl;
+    }
+
+    return 0;
+}
