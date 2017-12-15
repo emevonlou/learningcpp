f2cfa1faa1cd9e5c2c27703c9e48e3755adcb1cb
Emanuelle Costa <eme@vonlou.dev>
2017-12-15 13:03:24 -0300

    Arquivos

--- arquivos.cpp
+++ arquivos.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    ofstream output("saida.txt");
+    /*
+    ofstream output("saida.txt"); // pra gravar algo na saída
 
     output << "aprendendo c++" << endl;
+    * não imprime nada no terminal mas o arquivo vai estar em 'allfiles'
+    */
+
 
     return 0;
 }
