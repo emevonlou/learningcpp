83403d0571132d3a510356069aeeea891afa6781
Emanuelle Costa <eme@vonlou.dev>
2017-12-15 18:06:56 -0300

    Stack!

---
+++ stack.cpp
@@ -0,0 +1,26 @@
+/* 
+  -----Container Stack(Pilha)------
+    O primeiro elemento a entrar é o último a sair
+*/
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    stack<double> pilha;
+
+    pilha.push(3.14);
+    pilha.push(5.123);
+    pilha.push(10.56);
+
+    cout << "Topo: " << pilha.top() << endl;
+    cout << "Tamanho: " << pilha.size() << endl;
+
+    if(pilha.empty())
+        cout << "Pilha vazia." << endl;
+    else
+        cout << "Pilha não vazia." << endl;
+        
+    return 0;
+}
