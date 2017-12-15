27af9d19fdcebae91f18c3c762bc5a726937dd3e
Emanuelle Costa <eme@vonlou.dev>
2017-12-15 13:06:48 -0300

    Arquivos

--- arquivos.cpp
+++ arquivos.cpp
@@ -11,6 +11,21 @@ int main(int argc, char *argv[])
     * não imprime nada no terminal mas o arquivo vai estar em 'allfiles'
     */
 
+    ifstream in("saida.txt");
 
+    char c = in.get();
+    string texto;
+
+    texto.push_back(c);
+
+    while(in.good())
+    {
+        c = in.get();
+        texto.push_back(c);
+    }
+
+    cout << texto << endl;
+
+    
     return 0;
 }
