482ca4b753ca98008735eeaf3851343135205a49
Emanuelle Costa <eme@vonlou.dev>
2017-12-15 14:02:51 -0300

    Classe Vector

--- container.cpp
+++ container.cpp
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
     v.push_back(12);
     v.push_back(20);
     v.push_back(30);
+    // v.pop_back(); Deletar o ultimo elemento
 
     for(unsigned int i = 0; i < v.size(); i++)
     {
@@ -19,7 +20,11 @@ int main(int argc, char *argv[])
     if(v.empty()) // verificando se o vetor está vazio
         cout << "Vetor vazio." << endl;
     else
-        cout << "Vetor nao vazio." << endl; 
+        cout << "Vetor nao vazio." << endl;
+
+    cout << "Elemento da segunda posicao: " << v.at(1) << endl;  //Saber um determinado elemento do vetor 
+    cout << "Primeiro elemento: " << v.front() << endl; //verificar qual é o primeiro elemento
+    cout << "Ultimo elemento: " << v.back() << endl; // verificar qual foi o ultimo elemento
 
     return 0;
 }
