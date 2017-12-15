baded1f33f106bb082f7e7503e9fc40391786340
emevonlou <emanuellebc@gmail.com>
2017-12-15 13:12:32 -0300

    Arquivos

--- arquivos.cpp
+++ arquivos.cpp
@@ -5,27 +5,27 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     /*
-    ofstream output("saida.txt"); // pra gravar algo na saída
+    ofstream output("saida.txt"); // gravar algo na saída
 
     output << "aprendendo c++" << endl;
-    * não imprime nada no terminal mas o arquivo vai estar em 'allfiles'
+    * não imprime nada no terminal.
     */
 
-    ifstream in("saida.txt");
+    ifstream in("saida.txt"); // Lê o conteúdo de "saída.txt"
 
-    char c = in.get();
+    char c = in.get();  // pegando o primeiro caractere
     string texto;
 
-    texto.push_back(c);
+    texto.push_back(c);// adicionando o caractere no texto com push_back
 
-    while(in.good())
+    while(in.good())  // Enquanto eu puder ler
     {
         c = in.get();
-        texto.push_back(c);
+        texto.push_back(c); 
     }
 
     cout << texto << endl;
 
-    
+
     return 0;
 }
