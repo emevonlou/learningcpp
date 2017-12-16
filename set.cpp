801ce7ec20e200477041efeb58893e0ba1962b89
emevonlou <emanuellebc@gmail.com>
2017-12-16 13:06:18 -0300

    Set!

---
+++ set.cpp
@@ -0,0 +1,28 @@
+/*
+    -----set(conjunto)------
+    Não aceita elementos repetidos
+*/
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    set<int> conj;
+
+    conj.insert(10);
+    conj.insert(10);
+    conj.insert(20);
+    conj.insert(30);    
+
+    set<int>::iterator it;
+
+    for(it = conj.begin(); it !=  conj.end(); it++)
+    {
+        cout << *it << endl;
+    }
+
+    
+
+    return 0;
+}
