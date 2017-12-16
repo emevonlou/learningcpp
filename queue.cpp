7c90f59216dcca6c648a0158925b0f5e7961a81c
Emanuelle Costa <eme@vonlou.dev>
2017-12-16 12:17:14 -0300

    Queue-Fila

---
+++ queue.cpp
@@ -0,0 +1,25 @@
+/*
+    ------Queue(Fila)-----
+    O primeiro elemento que entra é o primeiro que sai.
+*/
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    queue<string> fila;
+
+    fila.push("dog");
+    fila.push("cat");
+    fila.push("fish");
+
+    fila.pop(); // remove sempre o primeiro elemento da fila
+
+    cout << "Primeiro elemento: " << fila.front() <<endl;
+    cout << "Tamanho da fila: " << fila.size() << endl;
+    cout << "Fila vazia?" << fila.empty() << endl;
+
+
+    return 0;
+}
