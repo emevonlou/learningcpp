00cb77938dc2aa7aaa2c07c30bb58e47bc28e9aa
Emanuelle Costa <eme@vonlou.dev>
2017-12-16 12:58:48 -0300

    Mapas!

---
+++ map.cpp
@@ -0,0 +1,29 @@
+/*
+    ----map(mapas)-----
+    Uma associação chave e valor.
+    Você acessa o valor através da chave, 
+    onde cada elemento é um par
+    composto pela chave e pelo valor.
+    Você mapeia um valor através da chave.
+*/
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main(int agrc, char *argv[])
+{
+    map<string, int> mapa;
+
+    mapa["Emanuelle"] = 38;
+    mapa["Joaquim"] = 7;
+    mapa["Joao"] = 19;
+
+    map<string, int>::iterator it;
+
+    for(it = mapa.begin(); it != mapa.end(); it++)
+    {
+        cout << "Idade do " << it->first << ":" << it->second << endl;
+    }
+
+    return 0;
+}
