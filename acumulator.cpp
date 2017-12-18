95a6eaa30b73754ddbcf95a93990f904f1b608e2
emevonlou <emanuellebc@gmail.com>
2017-12-18 13:41:54 -0300

    Make beautiful

--- acumulator.cpp
+++ acumulator.cpp
@@ -6,15 +6,15 @@ int main()  // função inteira
     int x, y,z,w,k;
     float soma;
 
-    cout << "Digite o primeiro numero: " << endl;
+    cout << "Digite o primeiro numero: ";
     cin >> x;
-    cout << "Digite o segundo numero: " << endl;
+    cout << "Digite o segundo numero: ";
     cin >> y;
-    cout << "Digite o terceiro numero: " << endl;
+    cout << "Digite o terceiro numero: ";
     cin >> z;
-    cout << "Digite o quarto numero: " << endl;
+    cout << "Digite o quarto numero: ";
     cin >> w;
-    cout << "Digite o quinto numero: " << endl;
+    cout << "Digite o quinto numero: ";
     cin >> k;
 
     soma = x + y + z + w + k;
