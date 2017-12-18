18fb644f8c307ef20be04d87643dca43fc57642f
emevonlou <emanuellebc@gmail.com>
2017-12-18 13:09:37 -0300

    Solving Problems

--- arquivos.cpp
+++ arquivos.cpp
@@ -11,14 +11,14 @@ int main(int argc, char *argv[])
     * não imprime nada no terminal.
     */
 
-    ifstream in("saida.txt"); // Lê o conteúdo de "saída.txt"
+    ifstream in("saida.txt"); // Lê o conteúdo de 'saída.txt' salvo no pc.
 
-    char c = in.get();  // Pegando o primeiro caractere
+    char c = in.get();  // Pegando o primeiro caractere.
     string texto;
 
-    texto.push_back(c);// adicionando o caractere no texto com push_back
+    texto.push_back(c);// Adicionando o caractere no texto com 'push_back'.
 
-    while(in.good())  // Enquanto eu puder ler
+    while(in.good())  // Enquanto eu puder ler com 'in.good()'
     {
         c = in.get();
         texto.push_back(c); 
