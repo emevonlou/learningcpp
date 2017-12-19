2d72ae0fe816fa5076d2b009f4f4ca9d78ce21bc
Emanuelle Costa <eme@vonlou.dev>
2017-12-19 14:56:36 -0300

    OPT

--- opt.cpp
+++ opt.cpp
@@ -6,7 +6,7 @@ int main(void)
 {
     int n;
 
-    cout << "Digite um numero.Vou te dizer se eh par ou impar: " << endl;
+    cout << "Digite um numero e te direi se eh par ou impar: " << endl;
     cin >> n;
 
     (n % 2 == 0)? cout << "Numero par." : cout << "Numero impar." << endl;
