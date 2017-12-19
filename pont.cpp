b7ccfe191ebdffdd1cc8419b0fc5de046b4d6922
emevonlou <emanuellebc@gmail.com>
2017-12-19 14:53:40 -0300

    for

--- pont.cpp
+++ pont.cpp
@@ -1,12 +1,24 @@
-/*
-----Ponteiros---
-
-
-*/
+// Pra um amigo que me fez pensar sobre caracteres.
 #include <iostream>
-#include <string>
-#include <stdio.h>
-#include <stdbool.h>
-
+#include <string.h>
 using namespace std;
 
+int main()
+{
+	char x = 's';
+	char y = 'n';
+	
+    cout << "Ola Amanda!" << endl;
+	cout << "Voce esta bem?(s/n)" << endl;
+	cin >> x;
+	if(x)
+	{
+		cout << "Que bom, fico feliz!" << endl;
+	}
+	else if(y)
+	{
+		cout << "O que aconteceu?" << endl;
+	}
+
+ return 0;
+}
