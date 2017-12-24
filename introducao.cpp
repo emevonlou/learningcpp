15f5803cdca47685473cc5ff692232e2297f2a15
emevonlou <emanuellebc@gmail.com>
2017-12-24 18:25:43 -0300

    Introdução update

--- introducao.cpp
+++ introducao.cpp
@@ -6,23 +6,23 @@ using namespace std;
 
 class Carro
 {   
-    private:  // protegendo os atributos com private
+    private:  // Protegendo os atributos com private
         string marca;
         int ano;
     public:
        void setMarca(string x)  
        { // Método com uma função que recebe a string como parâmetro
-           marca = x;  // mudando a marca pra x
+           marca = x;  // Mudando a marca pra x
        }
        string getMarca()
        {
-           return marca; // retornando a marca pra ela ser acessada pelo objeto e ser impressa
+           return marca; // Retornando a marca pra ela ser acessada pelo objeto e ser impressa
        }
        void setAno(int y)
        {
-           if(y > 1990) // se y for maior que 1990 o ano recebe y
+           if(y > 1990) // Se y for maior que 1990 o ano recebe y
                ano = y;
-           else // se o ano for menor que 1990, vou falaer que o ano é 1990
+           else // Se o ano for menor que 1990, vou falaer que o ano é 1990
                ano = 1990; 
        }
        int getAno()
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
     c2.setMarca("Ferrari");
     c2.setAno(2015);
     
-    cout << "Marca do primeiro carro: " << c1.getMarca() << endl; // me retorne a marca
+    cout << "Marca do primeiro carro: " << c1.getMarca() << endl; // Me retorne a marca
     cout << "Ano do primeiro carro: " << c1.getAno() << endl;
     cout << "Marca do segundo carro: " << c2.getMarca() << endl;
     cout << "Ano do segundo carro: " << c2.getAno() << endl;
