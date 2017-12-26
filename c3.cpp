c4c7388c593987d17c772aaad48db3452d4c853d
Emanuelle Costa <eme@vonlou.dev>
2017-12-26 17:52:05 -0300

    ponteiros conservadores!

--- c3.cpp
+++ c3.cpp
@@ -10,14 +10,21 @@ int main(int argc, char *argv[])
     */
 
     const int vet[3] = {1,2,3};
-    const int* p1;
-    int const* p2;
-    // int* const p3;
+    const int* p1 = &vet[0];
+    int const* p2 = &vet[1];
+    int* const p3 = new int[3]; // você deve inicializar quando declarado
 
-    p1 = &vet[0];
-    p2 = &vet[1];
+    cout << " *p1 = " << *p1 << endl;
+    cout << "*p2 = " << *p2 << endl;
 
-    
+    *p3 = 1;
+    *(p3 + 1) = 2;
+    *(p3 + 2) = 3;
+
+    cout << "p3: " << endl;
+    cout << *p3 << endl;
+    cout << *(p3 + 1) << endl;
+    cout << *(p3 + 2) << endl;
 
     return 0;
 }
