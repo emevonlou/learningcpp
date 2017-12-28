880e2a21e3db993cbcbdbba796f327b44bd1c3ed
emevonlou <emanuellebc@gmail.com>
2017-12-28 11:03:50 -0300

    update

--- charfunc2.cpp
+++ charfunc2.cpp
@@ -1,3 +1,4 @@
+// 66:
 #include <iostream>
 #include <string.h>
 #include <ctype.h>
@@ -7,48 +8,46 @@ int main(int argc, char *argv[])
 {
     char c = '2';
 
-    if(isalpha(c))
+    if (isalpha(c))
         cout << "Alfabetico!" << endl;
     else
         cout << "Nao alfabetico." << endl;
-    if(isdigit(c))
+    if (isdigit(c))
         cout << "Digito!" << endl;
     else
         cout << "Nao eh digito." << endl;
-    if(isupper(c))
+    if (isupper(c))
         cout << "Maiuscula!" << endl;
     else
         cout << "Minuscula." << endl;
-    if(islower(c))
+    if (islower(c))
         cout << "Minuscula!" << endl;
     else
         cout << "Maiuscula." << endl;
-    
+
     char nome[] = "Emanuelle";
     char nome2[] = "emanuelle";
     char sobrenome[] = "VonLou";
     char em[10];
 
-
     cout << "Tamanho: " << strlen(nome) << endl;
 
-    if(strcmp(nome, nome2) == 0)
+    if (strcmp(nome, nome2) == 0)
         cout << "Strings Iguais.\n";
     else
         cout << "Strings Diferentes." << endl;
 
     strcpy(em, nome);
-        cout << "Copiando de uma string pra outra:" << em << endl;
+    cout << "Copiando de uma string pra outra:" << em << endl;
 
     strcat(em, sobrenome);
-        cout << "Nome completo: " << em << endl;
+    cout << "Nome completo: " << em << endl;
 
     char my_char = 'a';
 
     char min_c = tolower(my_char);
 
     cout << "min_c: " << my_char << endl;
- 
 
     return 0;
 }
