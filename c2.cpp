401bc14a9f0781e98772fc93e14ecb0f85eaab04
Emanuelle Costa <eme@vonlou.dev>
2017-12-28 10:40:15 -0300

    update

--- c2.cpp
+++ c2.cpp
@@ -1,9 +1,10 @@
+// 102:
 #include <iostream>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    const double PI = 3.1415; 
+    const double PI = 3.1415;
     // variaveis constantes devem ser inicializadas quando criadas
 
     cout << PI << endl;
