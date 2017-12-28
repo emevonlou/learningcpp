f9c7f3ef96f98444c2453bb9186d9bc5063dd987
emevonlou <emanuellebc@gmail.com>
2017-12-28 10:48:06 -0300

    update

--- characnull.cpp
+++ characnull.cpp
@@ -1,17 +1,17 @@
-// variável char com chaves apresentando o caractere null
+// 24: variável char com chaves apresentando o caractere null
 #include <iostream>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    char nome[] = {'E','M','A','N','U','E','L','L','E','\0'}; // '\0' é um caractere null-considerado false
+    char nome[] = {'E', 'M', 'A', 'N', 'U', 'E', 'L', 'L', 'E', '\0'}; // '\0' é um caractere null-considerado false
     int i = 0;
 
-    while(nome[i])
+    while (nome[i])
     {
         cout << nome[i];
         cout << i++;
     }
-    
+
     return 0;
 }
