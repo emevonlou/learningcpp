813ea136d09c21425cceed57d42ac1809f076feb
emevonlou <emanuellebc@gmail.com>
2017-12-28 11:51:40 -0300

    comparing update

--- comparing.cpp
+++ comparing.cpp
@@ -1,6 +1,7 @@
-#include <iostream> // Entrada e saida de dados
-#include <cstdlib> // System
-#include <cstring> // Biblioteca de funções de manipulação com strings
+// 16:
+#include <iostream>  // Entrada e saida de dados
+#include <cstdlib>   // System
+#include <cstring>   // Biblioteca de funções de manipulação com strings
 using namespace std; // Declaração de espaço de nomes
 
 int main()
@@ -8,20 +9,23 @@ int main()
     char x1[20], x2[20];
 
     cout << "Digite uma palavra: ";
-    cin.getline(x1,20); 
+    cin.getline(x1, 20);
 
     cout << "Digite outra palavra: ";
-    cin.getline(x2,20);
+    cin.getline(x2, 20);
 
-    if(x1 > x2)
+    if (x1 > x2)
     {
-        cout << "\n" << x1 << "\t" << x2;
+        cout << "\n"
+             << x1 << "\t" << x2;
     }
     else
     {
-        cout << "\n" << x2 << "\t" << x1;
+        cout << "\n"
+             << x2 << "\t" << x1;
     }
-    cout << "\nx1:" << x1 << "\nx2: " << x2 << endl << endl;
+    cout << "\nx1:" << x1 << "\nx2: " << x2 << endl
+         << endl;
 
-   system("pause");
+    system("pause");
 }
