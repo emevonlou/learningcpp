81b3821a93dc2cc2e3078bfd4a4538b5bc3a61c1
Emanuelle Costa <eme@vonlou.dev>
2017-12-28 11:06:53 -0300

    update

--- charvector.cpp
+++ charvector.cpp
@@ -1,11 +1,12 @@
+// 63:
 #include <iostream>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     char nome[] = "Emanuelle Von Lou";
-    
-    for(int i = 0; nome[i] != '\0'; i++)
+
+    for (int i = 0; nome[i] != '\0'; i++)
     {
         cout << nome[i];
     }
