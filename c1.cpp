1735ba908a31504308e1afaea93319ab9e369ad9
emevonlou <emanuellebc@gmail.com>
2017-12-28 10:38:31 -0300

    update

--- c1.cpp
+++ c1.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     int var = 10; // Variável inteira
-    int* p1, * p2;  // ponteiros
+    int *p1, *p2; // ponteiros
 
     p1 = &var; // o ponteiro p1 e p2 guardam o endereço de 'var'
-    p2 = &var; 
+    p2 = &var;
 
     *p2 = 20;
     *p1 = 15;
