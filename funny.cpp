bd763c097016c221c382b509e693c0da53fe03db
emevonlou <emanuellebc@gmail.com>
2017-12-29 14:46:24 -0300

    make it simple

--- funny.cpp
+++ funny.cpp
@@ -1,10 +1,9 @@
-// Because the curious is the happiest!
+//  4: Because the curious is the happiest!
 
 #include <iostream>
 #include <cstdlib>
 #include <locale.h> //setlocale(LC_ALL, "")--- para acentos
 using namespace std;
-
 
 int main()
 {
@@ -12,10 +11,10 @@ int main()
 	int x;
 	cout << "You must enter 13 to exit: " << endl;
 	cin >> x;
-	while(x != 13)
+	while (x != 13)
 	{
-	   cout << "muahahaha!" << endl;
-	   cin >> x;  	
+		cout << "muahahaha!" << endl;
+		cin >> x;
 	}
 	system("pause");
 	return 0;
