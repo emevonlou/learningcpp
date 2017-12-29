38faee2c2f1dd1d13542328e48469b1e0423f0fa
Emanuelle Costa <eme@vonlou.dev>
2017-12-29 14:00:46 -0300

    Funny Update

--- decision.cpp
+++ decision.cpp
@@ -1,17 +1,17 @@
-// hey!!Não confundir operador de atribuição '=' com operador de igualdade '=='
+// 44: hey!!Não confundir operador de atribuição '=' com operador de igualdade '=='
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int n1,n2;
+    int n1, n2;
 
     cout << "Digite o primeiro numero: " << endl;
     cin >> n1;
     cout << "Digite o segundo numero: " << endl;
     cin >> n2;
 
-    if(n1 > n2)
+    if (n1 > n2)
     {
         cout << "O primeiro numero eh maior que o segundo!!" << endl;
     }
@@ -19,7 +19,7 @@ int main()
     {
         cout << "O primeiro numero eh menor que o segundo!!" << endl;
     }
-    if(n1 == n2)
+    if (n1 == n2)
     {
         cout << "O primeiro numero eh igual ao segundo!!" << endl;
     }
