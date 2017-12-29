94f656184344af1f4dbe0be2166eb4f195327aed
Emanuelle Costa <eme@vonlou.dev>
2017-12-29 13:26:48 -0300

    Funny Update

--- dc.cpp
+++ dc.cpp
@@ -1,11 +1,10 @@
-//Find the numbers corresponding to these characters.
+// 40:  Find the numbers corresponding to these characters.
 
 #include <stdio.h> // Entrada e saida de dados
 
-
 int main(void)
 {
-    for(int i = 0; i <= 300; i++)
+    for (int i = 0; i <= 300; i++)
     {
         printf("[%d]: %c\n", i, i);
     }
