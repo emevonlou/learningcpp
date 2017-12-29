1f007b72790b5b744845dcac3fb6b6c6cf5272b5
Emanuelle Costa <eme@vonlou.dev>
2017-12-29 13:25:34 -0300

    Days!

--- days.cpp
+++ days.cpp
@@ -1,21 +1,21 @@
+// 25:
 #include <iostream>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    for(int i = 1979; i <= 2017; i++)
+    for (int i = 1979; i <= 2017; i++)
     {
         cout << "Estamos no ano: " << i << endl;
         cout << "-----------------------------------------------" << endl;
-        for(int j = 1; j <= 12; j++)
+        for (int j = 1; j <= 12; j++)
         {
             cout << "Estamos no mes: " << j << endl;
             cout << "-----------------------------------------------" << endl;
-            for(int k = 1; k <= 30; k++)
+            for (int k = 1; k <= 30; k++)
             {
-            //    cout << "Estamos no dia: " << k << endl;
+                //    cout << "Estamos no dia: " << k << endl;
             }
         }
-
     }
 }
