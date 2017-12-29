acdb02f92813c9af26d888e1b104b7e1121701e0
emevonlou <emanuellebc@gmail.com>
2017-12-29 14:53:07 -0300

    make it simple

--- ifcomp.cpp
+++ ifcomp.cpp
@@ -1,36 +1,38 @@
+// 13:
 #include <iostream>
- using namespace std;
+using namespace std;
 
- int main(void){
- int idade;
- char sexo;
+int main(void)
+{
+  int idade;
+  char sexo;
 
- cout<<"Qual a sua idade?"<<endl;
- cin>>idade;
- cout<<"Digite (m)para Masculino, (f)para Feminino"<<endl;
- cin>>sexo;
+  cout << "Qual a sua idade?" << endl;
+  cin >> idade;
+  cout << "Digite (m)para Masculino, (f)para Feminino" << endl;
+  cin >> sexo;
 
- if (idade >= 18)
- {
- cout<<"Você pode dirigir"<<endl;
+  if (idade >= 18)
+  {
+    cout << "Você pode dirigir" << endl;
 
- if (sexo=='m')
- {
+    if (sexo == 'm')
+    {
 
-cout<<"Você precisa ir no Serviço Militar"<<endl;
- }
- }
- else 
- {
- if (idade > 16)
- {
- cout<<"Você já pode votar"<<endl;
- }
- else 
- {
-   cout<<"Você não pode votar nem dirigir"<<endl;
- }
- }
- 
- return 0;
- }
+      cout << "Você precisa ir no Serviço Militar" << endl;
+    }
+  }
+  else
+  {
+    if (idade > 16)
+    {
+      cout << "Você já pode votar" << endl;
+    }
+    else
+    {
+      cout << "Você não pode votar nem dirigir" << endl;
+    }
+  }
+
+  return 0;
+}
