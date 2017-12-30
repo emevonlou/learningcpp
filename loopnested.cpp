cd13b1392447e24be0d630b70dfbef4df4d23858
Emanuelle Costa <eme@vonlou.dev>
2017-12-30 18:00:54 -0300

    make it simple

--- loopnested.cpp
+++ loopnested.cpp
@@ -1,25 +1,25 @@
- // loop dentro de outro loop
- #include <iostream>
- using namespace std;
+// 55: loop dentro de outro loop
+#include <iostream>
+using namespace std;
 
- int main(int argc, char *argv[])
- {
-     int n = 1;
+int main(int argc, char *argv[])
+{
+    int n = 1;
 
-     while(n <= 10) // a cada interação do n, vamos contar 10 vezes o i
-     {
-         int i = 1;
+    while (n <= 10) // a cada interação do n, vamos contar 10 vezes o i
+    {
+        int i = 1;
 
         cout << "\nn: " << n << endl;
         n++;
 
-         while(i <= 10)
-         {
+        while (i <= 10)
+        {
             cout << "i: " << i << endl;
-            i++;    
-         }
+            i++;
+        }
         cout << endl;
-     }
+    }
 
-     return 0;
- }
+    return 0;
+}
