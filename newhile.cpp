316a723dc9b3c0f4e061618a2cb8ec5aba8c7d32
emevonlou <emanuellebc@gmail.com>
2017-12-30 18:20:37 -0300

    make it simple

--- newhile.cpp
+++ newhile.cpp
@@ -1,3 +1,4 @@
+// 
 #include <iostream>
 using namespace std;
 
@@ -5,12 +6,12 @@ int main(int argc, char *argv[])
 {
     int n = 0;
 
-    while(1) // enquanto for verdade 
+    while (1) // enquanto for verdade
     {
         n++;
-        if(n % 2 == 0) // se o numero for par
-             continue;
-        if(n > 100)
+        if (n % 2 == 0) // se o numero for par
+            continue;
+        if (n > 100)
             break;
         cout << n << endl;
     }
