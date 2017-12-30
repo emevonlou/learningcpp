0014ce3f7a23969c72a810ae1a4597ec4702acd4
Emanuelle Costa <eme@vonlou.dev>
2017-12-30 18:17:35 -0300

    make it simple

--- multiplesofsix.cpp
+++ multiplesofsix.cpp
@@ -1,17 +1,18 @@
-#include <iostream>  
+// 8:
+#include <iostream>
 #include <cstdlib>   // System
 using namespace std; // declaração de espaço de nomes- serve pra resolver conflitos de nomes
 
 int main()
 {
 	int num, c, conta6 = 0;
-	for(c= 1; c <=5; c++)
+	for (c = 1; c <= 5; c++)
 	{
 		cout << "\n digite numero: ";
 		cin >> num;
-		if(num % 6 == 0)
-		conta6 ++;
-        cout << "\n Total de multiplos de 6: " << conta6 << endl;
-    }
+		if (num % 6 == 0)
+			conta6++;
+		cout << "\n Total de multiplos de 6: " << conta6 << endl;
+	}
 	system("pause");
 }
