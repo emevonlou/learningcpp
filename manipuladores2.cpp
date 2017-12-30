c829d801522a4ebf4891e97c22945ab6a941a62a
Emanuelle Costa <eme@vonlou.dev>
2017-12-30 18:04:29 -0300

    make it simple

--- manipuladores2.cpp
+++ manipuladores2.cpp
@@ -1,6 +1,7 @@
+// 77:
 #include <iostream> // Traz um conjunto de instruções chamados de manipuladores.
 #include <stdlib.h> // system("pause")
-#include <iomanip> // setw()
+#include <iomanip>  // setw()
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -9,11 +10,11 @@ int main(int argc, char *argv[])
     //obj cout
 
     cout << "Estudando a entrada e saida de dados: " << endl; //OBJETO DE SAÍDA(cout)
-    cout << setw(10) << 1 << endl; // setw() Estabelece tamanho de um determinado campo.
+    cout << setw(10) << 1 << endl;                            // setw() Estabelece tamanho de um determinado campo.
     cout << setw(10) << 2 << endl;
     cout << setw(10) << 3 << endl;
     cout << setw(10) << 4 << endl;
 
-
     system("pause");
+    return 0;
 }
