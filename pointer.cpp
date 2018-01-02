5659bf8475e8aa2fc18a4f2fd7014c763c089a6a
emevonlou <emanuellebc@gmail.com>
2018-01-01 22:47:52 -0300

    make it simple

--- pointer.cpp
+++ pointer.cpp
@@ -1,16 +1,17 @@
+// 74:
 #include <iostream>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     int vet[] = {1, 2, 3, 4};
-    int* p_vet; // Ponteiro
-    int tam_vet = sizeof(vet)/sizeof(int); // calcula o tamanho do vetor
+    int *p_vet;                              // Ponteiro
+    int tam_vet = sizeof(vet) / sizeof(int); // calcula o tamanho do vetor
     int i = 0;
 
     p_vet = &vet[0]; // Ponteiro guarda o endereço da primeira variável.
 
-    while(i < tam_vet)
+    while (i < tam_vet)
     {
         cout << *p_vet << endl;
         p_vet++;
