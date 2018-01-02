1e6e4823f894de50a2caef2cd21850cf2bcc2b99
Emanuelle Costa <eme@vonlou.dev>
2018-01-02 17:54:36 -0300

    make it simple

--- regres.cpp
+++ regres.cpp
@@ -1,12 +1,12 @@
-// laço for com decremento.
+// 45: laço for com decremento.
 #include <iostream>
 using namespace std;
 
 int main(void)
 {
   cout << "contagem regressiva: " << endl;
-  for(int i = 9; i >= 0; i--)
+  for (int i = 9; i >= 0; i--)
   {
-      cout << i << endl;
+    cout << i << endl;
   }
 }
