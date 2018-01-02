6d8a8a5bcce289e3e5135e8cf1f4de804d847d80
emevonlou <emanuellebc@gmail.com>
2018-01-02 17:39:43 -0300

    make it simple

--- ponteiro8.cpp
+++ ponteiro8.cpp
@@ -1,11 +1,12 @@
+// 38:
 #include <iostream>
 using namespace std;
 
-int my_strlen(char* str)
+int my_strlen(char *str)
 {
     int tam = 0;
 
-    while(*str != '\0' )
+    while (*str != '\0')
     {
         str++;
         tam++;
@@ -13,26 +14,26 @@ int my_strlen(char* str)
     return tam;
 }
 
-char* my_strcat(char* dest, char* orig)
+char *my_strcat(char *dest, char *orig)
 {
-    char* resultado;
+    char *resultado;
     int tam_dest = my_strlen(dest);
     int tam_orig = my_strlen(orig);
 
     resultado = new char[tam_dest + tam_orig];
     char *p_str = resultado;
     resultado = dest;
-    while(*dest != '\0')
+    while (*dest != '\0')
     {
         *p_str = *dest;
-         p_str++;
-         dest++;
+        p_str++;
+        dest++;
     }
-    while(*orig != '\0')
+    while (*orig != '\0')
     {
         *p_str = *orig;
-         p_str++;
-         orig++;
+        p_str++;
+        orig++;
     }
     *p_str = '\0';
     return resultado;
@@ -40,15 +41,15 @@ char* my_strcat(char* dest, char* orig)
 
 int main(int argc, char *argv[])
 {
-    char* nome1 = new char[100];
-    char* nome2 = new char[100];
-    char* resultado = 0;
+    char *nome1 = new char[100];
+    char *nome2 = new char[100];
+    
 
     cout << "Digite o primeiro nome: ";
     cin >> nome1;
     cout << "Digite o segundo nome: ";
     cin >> nome2;
 
-    cout << "resultado:" << my_strcat(nome1,nome2) << endl;
+    cout << "resultado:" << my_strcat(nome1, nome2) << endl;
     return 0;
 }
