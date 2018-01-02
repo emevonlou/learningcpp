f1e9e25f4342fed1568b1873e2c6c1e2254b75c5
emevonlou <emanuellebc@gmail.com>
2018-01-01 22:35:54 -0300

    make it simple

--- obj2.cpp
+++ obj2.cpp
@@ -2,20 +2,18 @@
 #include <iostream>
 using namespace std;
 
-
 class Carro
 {
-public:
+  public:
 	int ano;
 	float peso;
-
 };
 
 int main(int argc, char *argv[])
 {
 	Carro carros[3];
 
-	carros[0].ano = 2015;  
+	carros[0].ano = 2015;
 	carros[0].peso = 756.45;
 	carros[1].ano = 2016;
 	carros[1].peso = 678.50;
@@ -24,9 +22,10 @@ int main(int argc, char *argv[])
 	carros[3].ano = 2009;
 	carros[3].peso = 900.48;
 
-	cout << "Imprimindo todos os carros: " << endl << endl;
+	cout << "Imprimindo todos os carros: " << endl
+		 << endl;
 
-	for(int i = 0; i < 3; i++)
+	for (int i = 0; i < 3; i++)
 	{
 		cout << "\nAno: " << carros[i].ano;
 		cout << "\nPeso: " << carros[i].peso << endl;
