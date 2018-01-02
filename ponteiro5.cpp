ca17cb2381c0cd15f0dd2dbefd03234009de7410
Emanuelle Costa <eme@vonlou.dev>
2018-01-02 17:35:59 -0300

    make it simple

--- ponteiro5.cpp
+++ ponteiro5.cpp
@@ -1,13 +1,14 @@
+// 35:
 #include <iostream>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    int array[] = {1,2,3,4,5};
-    int* parray = &array[0];
+    int array[] = {1, 2, 3, 4, 5};
+    int *parray = &array[0];
     int i = 0;
 
-    while(i < 5)
+    while (i < 5)
     {
         cout << *parray << endl;
         parray++;
