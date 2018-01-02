15c140ec8c7ded04c9d9bcef0f288239d80508cd
Emanuelle Costa <eme@vonlou.dev>
2018-01-01 22:42:05 -0300

    make it simple

--- para3.cpp
+++ para3.cpp
@@ -1,3 +1,4 @@
+// 50:
 #include <iostream>
 using namespace std;
 
@@ -5,14 +6,14 @@ int main()
 {
     int conta, num = 1, s = 0;
 
-for(conta = 10; conta <= 40; conta= conta+10)
-{
-     s= s + num;
-     num= num + 2;
-}
+    for (conta = 10; conta <= 40; conta = conta + 10)
+    {
+        s = s + num;
+        num = num + 2;
+    }
     cout << s << endl;
 
     cout << num << endl;
 
-return 0; 
+    return 0;
 }
