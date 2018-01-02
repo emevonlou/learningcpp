4306727cba1d9fb35a2ae77df03aa9e8caa1d14d
Emanuelle Costa <eme@vonlou.dev>
2018-01-01 22:40:11 -0300

    make it simple

--- para.cpp
+++ para.cpp
@@ -1,12 +1,14 @@
+// 48:
 #include <iostream>
 #include <cmath>
 using namespace std;
 
 int main()
 {
-    for(int i = 25;i > 0;i = i-6)
-    { 
-    cout << i << " ";
+    for (int i = 25; i > 0; i = i - 6)
+    {
+        cout << i << " ";
     }
-    system( "pause" );
+    system("pause");
+    return 0;
 }
