85a2f72fe0c807b1b6198814a931aa63d7c85856
Emanuelle Costa <eme@vonlou.dev>
2018-01-02 17:37:35 -0300

    make it simple

--- ponteiro7.cpp
+++ ponteiro7.cpp
@@ -1,13 +1,14 @@
+// 37:
 #include <iostream>
-#include <string.h> 
+#include <string.h>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    char nome[100] ;
+    char nome[100];
     int tam;
-    char* p;
-    char* pini;
+    char *p;
+    char *pini;
 
     cout << "Digite um nome: ";
     cin >> nome;
@@ -16,14 +17,14 @@ int main(int argc, char *argv[])
     p = &nome[tam - 1];
     pini = &nome[0];
 
-    while(true)
+    while (true)
     {
         cout << *p;
-        if(p == pini)
+        if (p == pini)
             break;
         p--;
     }
-    
+
     cout << endl;
     return 0;
 }
