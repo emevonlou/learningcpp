6a4cf7a46faf410ec46c4d4ac88229ed22c35f79
Emanuelle Costa <eme@vonlou.dev>
2018-01-02 17:59:12 -0300

    make it simple

--- sqrtsinlog.cpp
+++ sqrtsinlog.cpp
@@ -1,17 +1,16 @@
-// raiz quadrada, seno e logaritmo.
+// 10: raiz quadrada, seno e logaritmo.
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
 using namespace std;
 
-
 int main()
 {
 	system("color  1E"); // altera a cor
 	cout << "Raiz quadrada: " << sqrt(16) << endl;
-	cout << "seno 45 graus: " << sin(45*3.14159625/180) << endl;
-	cout << "logaritmo de 64 na base 2: " << log(64)/log(2) << endl;
+	cout << "seno 45 graus: " << sin(45 * 3.14159625 / 180) << endl;
+	cout << "logaritmo de 64 na base 2: " << log(64) / log(2) << endl;
 	system("pause");
-	
+
 	return 0;
 }
