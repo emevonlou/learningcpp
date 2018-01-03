7f0a23e946818d4012deb64d75d5efc385f254c2
Emanuelle Costa <eme@vonlou.dev>
2018-01-03 11:42:07 -0300

    make it simple

--- structs1.cpp
+++ structs1.cpp
@@ -1,14 +1,14 @@
-//structs: Tem por objetivo agrupar os dados.
+// 79: structs: Tem por objetivo agrupar os dados.
 #include <iostream>
 #include <string.h> // strcpy()
 using namespace std;
 
-typedef struct pessoa  //Definindo um nome para 'struct pessoa' com typedef
+typedef struct pessoa //Definindo um nome para 'struct pessoa' com typedef
 {
     char nome[100];
     int idade;
     float peso;
-}tipo_pessoa; // ao invés de digitar sempre 'struct pessoa', digita 'tipo_pessoa'
+} tipo_pessoa; // ao invés de digitar sempre 'struct pessoa', digita 'tipo_pessoa'
 
 int main(int argc, char *argv[])
 {
@@ -21,6 +21,6 @@ int main(int argc, char *argv[])
     cout << "Nome: " << p.nome << endl;
     cout << "Idade: " << p.idade << endl;
     cout << "Peso: " << p.peso << endl;
-    
+
     return 0;
 }
