c661b933b3e69ed4a00a806f03037ac56353a9be
emevonlou <emanuellebc@gmail.com>
2018-01-03 11:45:47 -0300

    make it simple

--- this.cpp
+++ this.cpp
@@ -1,35 +1,36 @@
-// Arquivos de cabeçalho
+// 97: Arquivos de cabeçalho
 #include <iostream>
 #include <string>
 #include <cstdlib>
 using namespace std;
 
 class Carro
-{   
-    private:  
-        string marca;
-        int ano;
-    public:
-       void setMarca(string marca)  // Essa marca é uma variável que veio como parametro
-       { 
-           this->marca = marca; // Essa marca é um atributo da classe
-           // a marca da classe vai receber a marca que chegou como parametro
-       }
-       string getMarca()
-       {
-           return marca; 
-       }
-       void setAno(int ano)
-       {
-           if(ano > 1990) 
-               this->ano = ano; // O ano da classe recebe o ano;
-           else 
-              this->ano = 1990; // caso contrário, o ano recebe 1990; 
-       }
-       int getAno()
-       {
-           return ano;
-       }
+{
+  private:
+    string marca;
+    int ano;
+
+  public:
+    void setMarca(string marca) // Essa marca é uma variável que veio como parametro
+    {
+        this->marca = marca; // Essa marca é um atributo da classe
+        // a marca da classe vai receber a marca que chegou como parametro
+    }
+    string getMarca()
+    {
+        return marca;
+    }
+    void setAno(int ano)
+    {
+        if (ano > 1990)
+            this->ano = ano; // O ano da classe recebe o ano;
+        else
+            this->ano = 1990; // caso contrário, o ano recebe 1990;
+    }
+    int getAno()
+    {
+        return ano;
+    }
 };
 
 int main(int argc, char *argv[])
@@ -42,7 +43,7 @@ int main(int argc, char *argv[])
     Carro c2;
     c2.setMarca("Ferrari");
     c2.setAno(2015);
-    
+
     cout << "Marca do primeiro carro: " << c1.getMarca() << endl; // me retorne a marca
     cout << "Ano do primeiro carro: " << c1.getAno() << endl;
     cout << "Marca do segundo carro: " << c2.getMarca() << endl;
