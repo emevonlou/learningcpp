9e1a3ae3dcb813746ef88b99ff260e212e8c2ec3
Emanuelle Costa <eme@vonlou.dev>
2018-01-03 11:51:16 -0300

    make it simple

--- while.cpp
+++ while.cpp
@@ -1,20 +1,21 @@
+// 12:
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	int n; 
-	
+	int n;
+
 	cout << "Digite um numero: " << endl;
 	cin >> n;
-	
-	while(n <=100)
+
+	while (n <= 100)
 	{
-		if (n > 20 ) 
-	    break;
-	    cout << n << endl;
-	    n++; 
-	}	
+		if (n > 20)
+			break;
+		cout << n << endl;
+		n++;
+	}
 	return 0;
 }
