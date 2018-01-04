ec93860ba098f4cd7124c8bb790c7a295c021b56
Emanuelle Costa <eme@vonlou.dev>
2018-01-04 14:47:06 -0300

    Making beautiful

--- vecnew.cpp
+++ vecnew.cpp
@@ -6,13 +6,13 @@ int main()
 {
     int matriz[2][2], i, j;
 
-    // Passando valores
+    // Passando valores.
     matriz[0][0] = 1;
     matriz[0][1] = 2;
     matriz[1][0] = 3;
     matriz[1][1] = 4;
 
-    //imprimindo na tela
+    // Imprimindo na tela.
     for (i = 0; i < 2; i++)
     {
         for (j = 0; j < 2; j++)
