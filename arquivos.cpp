0da0d3a79ed06060837d06cf52b2e98bc2f9938c
emevonlou <emanuellebc@gmail.com>
2018-01-04 14:49:37 -0300

    Making beautiful

--- arquivos.cpp
+++ arquivos.cpp
@@ -6,10 +6,9 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     /*
-    ofstream output("saida.txt"); // gravar algo na saída
-
-    output << "aprendendo c++" << endl;
-    * não imprime nada no terminal.
+        ofstream output("saida.txt"); // Gravar algo na saída.
+        output << "aprendendo c++" << endl;
+        * não imprime nada no terminal.
     */
 
     ifstream in("saida.txt"); // Lê o conteúdo de 'saída.txt' salvo no pc.
