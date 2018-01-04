a4ec0875f2672145bb425b0cdf8be724cdea8eab
emevonlou <emanuellebc@gmail.com>
2018-01-04 15:01:33 -0300

    Making beautiful

--- classe.cpp
+++ classe.cpp
@@ -1,12 +1,12 @@
-// 81: Programando com classe(agrupamento de dados com classe)
+// 81: Programando com classe(agrupamento de dados com classe).
 #include <iostream>
 using namespace std;
 
-class Pessoa // nome da classe com letra maiúscula
+class Pessoa // Nome da classe com letra maiúscula.
 {
   private:
-    /*
-    *modificador de acesso.
+/*
+    Modificador de acesso.
     Deve ser 'private' para garantir a segurança de alterações acidentais.
     Sendo somente acessíveis essas variáveis através de métodos,
     Contribuindo no encapsulamento de dados.
@@ -15,10 +15,10 @@ class Pessoa // nome da classe com letra maiúscula
     int peso;
 
   public:
-    // métodos
+    // Métodos
     void setIdade(int idade)
     {
-        this->idade = idade; // this = referência a variável dessa instância que vai receber o parâmetro 'idade'
+        this->idade = idade; // This = referência a variável dessa instância que vai receber o parâmetro 'idade'.
     }
 
     int getIdade()
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 {
     Pessoa pessoa;
 
-    pessoa.setIdade(38); // Acesso aos membros do objeto
+    pessoa.setIdade(38); // Acesso aos membros do objeto.
     pessoa.setPeso(44.3);
 
     cout << "Idade: " << pessoa.getIdade() << endl;
