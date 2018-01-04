10ac435eb8739dc57e5b47f1ec653dd90c0cf4c3
Emanuelle Costa <eme@vonlou.dev>
2018-01-04 14:54:12 -0300

    Making beautiful

--- caracnull.cpp
+++ caracnull.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 void inverte(char nome[])
 {
-    // obtendo o tamanho da string
+    // Obtendo o tamanho da string.
     int tam;
-    // Eu posso ter um loop sem chaves
+    // Eu posso ter um loop sem chaves.
     for (tam = 0; nome[tam]; tam++)
     {
-        // string invertida
+        // string invertida.
         for (int i = tam - 1; i >= 0; i--)
             cout << nome[i];
     }
@@ -18,7 +18,7 @@ void inverte(char nome[])
 int main(int argc, char *argv[])
 {
     char nome[] = "Emanuelle";
-    // chama a função
+    // Chama a função.
     inverte(nome);
     return 0;
 }
