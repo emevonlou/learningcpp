d0914fe7f608d86c38a7c0fa749eb88c7d024d56
Emanuelle Costa <eme@vonlou.dev>
2018-01-04 15:01:33 -0300

    Making beautiful

--- classestring.cpp
+++ classestring.cpp
@@ -1,10 +1,10 @@
 /*
 106:
-	classe string
+	Classe string.
 	Funcoes da classe string: size()
 */
 #include <iostream>
-#include <string> // classe string
+#include <string> // Classe string.
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -14,22 +14,22 @@ int main(int argc, char *argv[])
 	cout << "Tamanho da string: " << nome1.size() << endl;
 	cout << "primeiro caractere da string: " << nome1[0] << endl;
 
-	//Adicionando strings com .append
+	// Adicionando strings com .append
 	nome1.append("Costa");
 
-	string nome("Joaquim Vincent"); // Passando pro construtor
-	//inserindo string com .insert()
+	string nome("Joaquim Vincent"); // Passando pro construtor.
+	// inserindo string com .insert()
 	nome.insert(nome.size(), "Frota");
 
 	cout << "Segunda string: " << nome << endl;
 	cout << "Tamanho da segunda string: " << nome.size() << endl;
 
-	nome1.clear(); // limpa a string nome1
+	nome1.clear(); // Limpa a string nome1.
 
 	cout << "Tamanho da string depois do .clear(): " << nome.size() << endl;
-	// o tamanho da string ser� zero
+	// O tamanho da string sera zero.
 
-	// verificando se a string est� vazia com empty
+	// Verificando se a string esta vazia com empty.
 	if (nome1.empty())
 		cout << "String vazia." << endl;
 	else
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
 	// Acessando o segundo caractere com .at()
 	cout << "Acessando o segundo caractere: " << nome.at(1) << endl;
 
-	// adicionando strings
+	// Adicionando strings.
 	nome.push_back('F');
 	cout << "novo nome: " << nome << endl;
 
