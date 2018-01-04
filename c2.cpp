69d67275fd5e51806d9188168250127907244784
Emanuelle Costa <eme@vonlou.dev>
2018-01-04 14:51:22 -0300

    Making beautiful

--- c2.cpp
+++ c2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     const double PI = 3.1415;
-    // variaveis constantes devem ser inicializadas quando criadas
+    // Variaveis constantes devem ser inicializadas quando criadas.
 
     cout << PI << endl;
 
