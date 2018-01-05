5180b323c6fa3111c330a9a1babb5e6b47ffe7b2
Emanuelle Costa <eme@vonlou.dev>
2018-01-05 18:04:30 -0300

    Argumentos da função main

--- str.cpp
+++ str.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    char pessoas[3][100];
+    char pessoas[3][100] = {"Emanuelle", "Joaquim", "Helio"}; 
+    // 3 pessoas e a quantid maxima do nome de cada pessoa é 100 caract
+    int idade[3] = {26, 30, 20};
 
+    cout << "Nome: " << pessoas[0] << endl;
 
  return 0;   
 }
