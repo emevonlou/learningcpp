0318d4122be98f104a85921a68ecbed881724cea
emevonlou <emanuellebc@gmail.com>
2018-01-05 17:04:02 -0300

    Argumentos da função main

--- arraypont.cpp
+++ arraypont.cpp
@@ -7,6 +7,8 @@ int main(int argc, char *argv[])
     int* parray[3] = {&n1, &n2, &n3}; // declarando array de 3 ponteiros para inteiros
 
     cout << *parray[0] << endl;
+    cout << *parray[1] << endl;
+    cout << *parray[2] << endl;
 
 return 0;
 }
