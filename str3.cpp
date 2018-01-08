bc0c564221d85c6b467645f0af3129132daa5974
Emanuelle Costa <eme@vonlou.dev>
2018-01-08 15:11:22 -0300

    Agrupando dados com structs

--- str3.cpp
+++ str3.cpp
@@ -7,13 +7,13 @@ typedef struct pessoa
 {
     char nome[100];
     int idade;
-}t_pessoa;
+} t_pessoa;
 
 int main(int argc, char *argv[])
 {
     t_pessoa p[3];
 
-    strcpy(p[0].nome, "Emanuelle");
+    strcpy(p[0].nome, "Emanuelle"); // acessa a struct atraves do ponto
     p[0].idade = 38;
 
     strcpy(p[1].nome, "Joaquim");
@@ -22,14 +22,12 @@ int main(int argc, char *argv[])
     strcpy(p[2].nome, "Fifi");
     p[2].idade = 6;
 
-
     cout << "Nome 1: " << p[0].nome << endl;
     cout << "Idade 1: " << p[0].idade << endl;
     cout << "Nome 2: " << p[1].nome << endl;
     cout << "Idade 2: " << p[1].idade << endl;
     cout << "Nome 3: " << p[2].nome << endl;
     cout << "Idade 3: " << p[2].idade << endl;
-
 
     return 0;
 }
