486fe01c6976da18cb1f702181f81adbf2a3dd30
Emanuelle Costa <eme@vonlou.dev>
2018-01-09 13:11:20 -0300

    Construtores

--- arraypont.cpp
+++ arraypont.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     int n1 = 10, n2 = 20, n3 = 30;
-    int* parray[3] = {&n1, &n2, &n3}; // declarando array de 3 ponteiros para inteiros
+    int *parray[3] = {&n1, &n2, &n3}; // declarando array de 3 ponteiros para inteiros
 
     cout << *parray[0] << endl;
     cout << *parray[1] << endl;
     cout << *parray[2] << endl;
 
-return 0;
+    return 0;
 }
