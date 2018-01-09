48dded8d215bc8c110682433883f98eae210da15
emevonlou <emanuellebc@gmail.com>
2018-01-09 12:13:56 -0300

    Agrupando dados em structs

--- str5.cpp
+++ str5.cpp
@@ -5,11 +5,24 @@ using namespace std;
 struct Pessoa
 {
     int idade;
+    int numero_sorte;
 
-    Pessoa(int idade) // construtor
+    Pessoa(int idade) // primeiro construtor
     {
         this->idade = idade;
     }
+
+    Pessoa(int idade, int num_sorte) // segundo construtor
+    {
+        this->idade = idade;
+        this->numero_sorte = num_sorte;
+    }
+
+    void setIdade(int idade)
+    {
+        this->idade = idade;
+    }
+
     int getIdade()
     {
         return this->idade;
@@ -20,6 +33,7 @@ struct Pessoa
 int main(int argc, char *argv[])
 {
     Pessoa p(20);
+    Pessoa p2(20, 10);
 
     cout << p.getIdade() << endl;
 
