7c4128ab7cad570f9a8bc3e8abdfe4629637cc90
emevonlou <emanuellebc@gmail.com>
2018-01-10 12:52:08 -0300

    structs update

--- str2.cpp
+++ str2.cpp
@@ -7,6 +7,8 @@ typedef struct pessoa
 {
     char nome[100];
     int idade;
+    int ano;
+    char cpf[20];
 
 } t_pessoa;
 
@@ -19,6 +21,9 @@ int main(int argc, char *argv[])
 
     cout << "Nome: " << p.nome << endl;
     cout << "Idade: " << p.idade << endl;
+    cout << "Digite seu CPF: " << endl;
+    cin >> p.cpf;
+    cout << "Seu CPF eh: " << p.cpf << endl;
 
     return 0;
 }
