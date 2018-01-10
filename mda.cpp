78af5f499e87ddc2a58549b7234d34ec9e84c067
emevonlou <emanuellebc@gmail.com>
2018-01-10 19:56:27 -0300

    Parametros adicionais

--- mda.cpp
+++ mda.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 using namespace std;
 
-int quad(int num = 1) // valor default = 1
+int quad(int num) // valor default = 1
 {
     return num * num;
 }
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 {
     int num = 10;
 
-    cout << "Quadrado: " << quad() << endl; // func sem parametros
+    cout << "Quadrado: " << quad(num) << endl; // func sem parametros
 
     return 0;
 }
