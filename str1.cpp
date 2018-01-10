d5d3d9462f1ba59eed24baa36463a0f7136ac4bd
Emanuelle Costa <eme@vonlou.dev>
2018-01-10 12:41:58 -0300

    structs update

--- str1.cpp
+++ str1.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     char nomes[3][100] = {"Joaquim", "Fifi", "Eme"};
-    int idade[3] = {26, 30, 20};
+    int idades[3] = {26, 30, 20};
 
     cout << "Nome: " << nomes[0] << endl;
-    cout << "Idade: " << idade[0] << endl;
+    cout << "Idade: " << idades[0] << endl;
 
     return 0;
 }
