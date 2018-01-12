d0c2e81ecc6f18264c87d705308cef33bee47788
emevonlou <emanuellebc@gmail.com>
2018-01-12 16:39:22 -0300

    Parametros opcionais

--- mda.cpp
+++ mda.cpp
@@ -1,13 +1,13 @@
 /*
     129:
-    Parametros opcionais
-    Se nenhum parametro não foi passado
-    vocẽ pode ter/passar um valor default
+    Parametros opcionais:
+    Se nenhum parametro não foi passado,
+    vocẽ pode ter/passar um valor default.
 */ 
 #include <iostream>
 using namespace std;
 
-int quad(int num) // valor default = 1
+int quad(int num) // valor default = 1.
 {
     return num * num;
 }
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 {
     int num = 10;
 
-    cout << "Quadrado: " << quad(num) << endl; // func sem parametros
+    cout << "Quadrado: " << quad(num) << endl; // funcao sem parametros.
 
     return 0;
 }
