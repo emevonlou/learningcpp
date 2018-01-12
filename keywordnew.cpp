550109b4f1a26d6d51feed8177e1b40649289b2b
Emanuelle Costa <eme@vonlou.dev>
2018-01-12 16:28:15 -0300

    keyWordNew

--- keywordnew.cpp
+++ keywordnew.cpp
@@ -1,10 +1,10 @@
-//Com a palavra-chave 'new' você consegue alocar memória
+//Com a palavra-chave 'new' você consegue alocar memória.
 #include <iostream>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    int *vet = new int[10]; // Alocando espaço na memoria pra dez inteiros
+    int *vet = new int[10]; // Alocando espaço na memoria pra dez inteiros.
 
     *(vet + 0) = 10;
     cout << *(vet + 0) << endl;
