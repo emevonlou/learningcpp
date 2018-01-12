74606960f5832b91ce23c600e57ac17259115bde
Emanuelle Costa <eme@vonlou.dev>
2018-01-12 16:32:35 -0300

    Mais um loop

--- loopagain.cpp
+++ loopagain.cpp
@@ -4,9 +4,11 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    for (int i = 1; i <= 100; i++)
+    for (int i = 0; i <= 100; i++)
     {
         cout << "If I can help you, count on me." << endl;
     }
+    cout << "Buddy!" << endl;
+
     return 0;
 }
