1efc23fdbf673b93fd69d22bd3aae9deae5f9636
emevonlou <emanuellebc@gmail.com>
2018-01-12 16:20:35 -0300

    function inline

--- inline.cpp
+++ inline.cpp
@@ -7,8 +7,14 @@ inline void Eme()  // Funcoes inline cobram mais espaco na memoria.
     cout << "Learning C++" << endl;
 }
 
+inline void message()
+{
+    cout << "I really in love with that!" << endl;
+}
+
 int main(int argc, char *argv[])
 {
     Eme();
+    message();
     return 0;
 }
