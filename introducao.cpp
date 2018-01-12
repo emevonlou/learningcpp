bd8767c0b85d4f4a0abb4690bd9876af75472bb9
Emanuelle Costa <eme@vonlou.dev>
2018-01-12 16:25:36 -0300

    Encapsulamento

--- introducao.cpp
+++ introducao.cpp
@@ -17,13 +17,13 @@ class Carro
     }
     string getMarca()
     {
-        return marca; // Retornando a marca pra ela ser acessada pelo objeto e ser impressa
+        return marca; // Retornando a marca pra ela ser acessada pelo objeto e ser impressa.
     }
     void setAno(int y)
     {
-        if (y > 1990) // Se y for maior que 1990 o ano recebe y
+        if (y > 1990) // Se y for maior que 1990 o ano recebe y.
             ano = y;
-        else // Se o ano for menor que 1990, vou falaer que o ano é 1990
+        else // Se o ano for menor que 1990, vou falaer que o ano é 1990.
             ano = 1990;
     }
     int getAno()
@@ -36,14 +36,14 @@ int main(int argc, char *argv[])
 {
 
     Carro c1;
-    c1.setMarca("Fiat"); // Passando 'Fiat' atraves do metodo setMarca
+    c1.setMarca("Fiat"); // Passando 'Fiat' atraves do metodo setMarca.
     c1.setAno(2017);
 
     Carro c2;
     c2.setMarca("Ferrari");
     c2.setAno(2015);
 
-    cout << "Marca do primeiro carro: " << c1.getMarca() << endl; // Me retorne a marca
+    cout << "Marca do primeiro carro: " << c1.getMarca() << endl; // Me retorne a marca.
     cout << "Ano do primeiro carro: " << c1.getAno() << endl;
     cout << "Marca do segundo carro: " << c2.getMarca() << endl;
     cout << "Ano do segundo carro: " << c2.getAno() << endl;
