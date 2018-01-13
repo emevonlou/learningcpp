9d9be5c7b5b58b3b28da340cfdec51d575fd2833
Emanuelle Costa <eme@vonlou.dev>
2018-01-13 12:10:36 -0300

    stacks and Heap

--- memory2.cpp
+++ memory2.cpp
@@ -2,7 +2,7 @@
     73:
     A memória em c++ é dividida em duas partes: 'stacks' e 'Heap'.
     Todas as variáveis declaradas dentro da função, irão ocupar
-    memória da 'stack'.A 'Heap' é um espaço de memória que é utilizada
+    memória da 'stack'. A 'Heap' é um espaço de memória que é utilizada
     pra alocar memória dinamicamente durante a execução do programa.
 */
 #include <iostream>
@@ -10,13 +10,13 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    double *num = NULL; //Ponteiro inicializado com NULL
+    double *num = NULL; // Ponteiro inicializado com NULL.
 
-    num = new double;     // Requer memória para a variável
-    *num = 10;            // Atribui valor
-    cout << *num << endl; // Exibe o valor
+    num = new double;     // Requer memória para a variável.
+    *num = 10;            // Atribui valor.
+    cout << *num << endl; // Exibe o valor.
 
-    delete num; // Libera memória
+    delete num; // Libera memória.
 
     return 0;
 }
