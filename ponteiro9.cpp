8edb2ff9fa67717d8c2e42ed0a43e7862a8e4967
emevonlou <emanuellebc@gmail.com>
2018-01-13 12:26:43 -0300

    Ponteiros

--- ponteiro9.cpp
+++ ponteiro9.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     int n = 10;
     int *pont;
 
-    pont = &n; // 'Pega o endereço da variável n(int n = 10) e guarda em pont'
+    pont = &n; // 'Pega o endereço da variável n(int n = 10) e guarda em pont'.
 
     cout << *pont << endl;
     return 0;
