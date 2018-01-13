301135bf6298e7f29da0e977f56987d452ef86ff
Emanuelle Costa <eme@vonlou.dev>
2018-01-13 12:23:56 -0300

    Ponteiros

--- ponteiro.cpp
+++ ponteiro.cpp
@@ -1,14 +1,14 @@
 // 28:
-#include <iostream>  // entrada e saida de dados
-using namespace std; // declarando espaço de nome std padrao
+#include <iostream>  // Entrada e saida de dados.
+using namespace std; // Declarando espaço de nome std padrao.
 
 int main(int argc, char *argv[])
 {
     int var = 10;
-    int *pvar; // ponteiro
+    int *pvar; // Ponteiro.
 
-    // sempre inicializar um ponteiro antes de utiliza-lo
-    pvar = &var;           // &var é o endereço da variável var
+    // Sempre inicializar um ponteiro antes de utiliza-lo.
+    pvar = &var;           // &var é o endereço da variável var.
     cout << *pvar << endl; // acessa o conteúdo da variável var atraves do ponteiro
     *pvar = 20;            // muda o valor da variável através do ponteiro
     cout << var << endl;
