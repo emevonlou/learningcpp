db2e546c5327698e71b691a9b31e3918ecb0f589
emevonlou <emanuellebc@gmail.com>
2018-01-13 12:22:49 -0300

    Vetores e ponteiros

--- pointer.cpp
+++ pointer.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     int vet[] = {1, 2, 3, 4};
-    int *p_vet;                              // Ponteiro
-    int tam_vet = sizeof(vet) / sizeof(int); // calcula o tamanho do vetor
+    int *p_vet;                              // Ponteiro.
+    int tam_vet = sizeof(vet) / sizeof(int); // calcula o tamanho do vetor.
     int i = 0;
 
     p_vet = &vet[0]; // Ponteiro guarda o endereço da primeira variável.
