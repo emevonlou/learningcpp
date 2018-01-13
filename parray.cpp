1b0926ec3cda7722bbd86df2fec7bfdd32f04f1a
Emanuelle Costa <eme@vonlou.dev>
2018-01-13 12:21:41 -0300

    Alocando,desalocando memoria

--- parray.cpp
+++ parray.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    int *parray = new int[10]; // aloca memoria
+    int *parray = new int[10]; // Aloca memoria.
 
     cout << "Digite um numero: ";
     cin >> *(parray);
     cout << "Voce digitou: " << *(parray) << endl;
 
-    delete[] parray; // desaloca memoria
+    delete[] parray; // Desaloca memoria.
 
-    parray = NULL; // zerando o ponteiro
+    parray = NULL; // Zerando o ponteiro.
 
     return 0;
 }
