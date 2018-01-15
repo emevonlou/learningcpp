acf5c96fa4a8fc46d78364b7f2a9e66f1e80d2c0
Emanuelle Costa <eme@vonlou.dev>
2018-01-15 15:24:23 -0300

    arquivos de cabeçalho

--- this.cpp
+++ this.cpp
@@ -1,4 +1,4 @@
-// 97: Arquivos de cabeçalho
+// 97: Arquivos de cabeçalho.
 #include <iostream>
 #include <string>
 #include <cstdlib>
@@ -11,10 +11,10 @@ class Carro
     int ano;
 
   public:
-    void setMarca(string marca) // Essa marca é uma variável que veio como parametro
+    void setMarca(string marca) // Essa marca é uma variável que veio como parametro.
     {
-        this->marca = marca; // Essa marca é um atributo da classe
-        // a marca da classe vai receber a marca que chegou como parametro
+        this->marca = marca; // Essa marca é um atributo da classe.
+        // A marca da classe vai receber a marca que chegou como parametro.
     }
     string getMarca()
     {
@@ -37,14 +37,14 @@ int main(int argc, char *argv[])
 {
 
     Carro c1;
-    c1.setMarca("Fiat"); // Passando 'Fiat' atraves do metodo setMarca
+    c1.setMarca("Fiat"); // Passando 'Fiat' atraves do metodo setMarca.
     c1.setAno(2017);
 
     Carro c2;
     c2.setMarca("Ferrari");
     c2.setAno(2015);
 
-    cout << "Marca do primeiro carro: " << c1.getMarca() << endl; // me retorne a marca
+    cout << "Marca do primeiro carro: " << c1.getMarca() << endl; // me retorne a marca.
     cout << "Ano do primeiro carro: " << c1.getAno() << endl;
     cout << "Marca do segundo carro: " << c2.getMarca() << endl;
     cout << "Ano do segundo carro: " << c2.getAno() << endl;
