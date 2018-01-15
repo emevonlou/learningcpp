bc4b55beeab7f44e5616c22a13a1e1305dacabe1
emevonlou <emanuellebc@gmail.com>
2018-01-15 15:01:32 -0300

    Passagem por referencia

--- poref.cpp
+++ poref.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 void fun(int *n) // Variável que armazena endereço(ponteiro).
 {
-    *n = 20; // n está armazenando o endereço e modifica o valor da variável pra 20
+    *n = 20; // n está armazenando o endereço e modifica o valor da variável pra 20.
 }
 
 int main(int argc, char *argv[])
 {
-    int n = 10; // variável n recebe valor inicial 10
+    int n = 10; // Variável n recebe valor inicial 10.
 
-    fun(&n); // passando na chamada da função o endereço da variável
+    fun(&n); // Passando na chamada da função o endereço da variável.
 
     cout << n << endl;
 
