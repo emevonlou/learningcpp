0570afef016c45176ab438bcb0d86c7598e681f0
Emanuelle Costa <eme@vonlou.dev>
2018-01-15 15:14:44 -0300

    Raiz quadrada,seno e log

--- sizestring.cpp
+++ sizestring.cpp
@@ -1,11 +1,11 @@
 // 23:
 #include <iostream>
-#include <string.h> // função strlen();
+#include <string.h> // Função strlen();
 using namespace std;
 
 void inverte(char nome[])
 {
-    // função que calcula o tamanho da string.
+    // Função que calcula o tamanho da string.
     int tam = strlen(nome);
 
     // Eu posso ter um loop sem chaves!!
@@ -16,6 +16,6 @@ void inverte(char nome[])
 int main(int argc, char *argv[])
 {
     char nome[] = "Emanuelle";
-    inverte(nome); // chama a função
+    inverte(nome); // Chama a função.
     return 0;
 }
