7079b9bd65f9f3727395cbcdd09e4222f5c08aeb
Emanuelle Costa <eme@vonlou.dev>
2018-01-16 15:53:01 -0300

    Fstream!

--- arq3.cpp
+++ arq3.cpp
@@ -12,10 +12,18 @@ int main(int argc, char *argv[])
     string texto;
     // classe string redimenciona automaticamente para a quant de dados necessaria
     char c = in.get();  // metodo 'get' retorna o proximo caractere
-    texto.push_back(c); // Insere o caractere na string
+    texto.push_back(c); // Insere o caractere na string dando um push_back(c)
 
-    cout << "\nMostrando cada caracere:\n"
-         << endl;
+    cout << "\nMostrando cada caractere:\n" << endl;
+
+    // Loop pra percorrer o arquivo e mostrar caractere por caractere
+    while(in.good()) // Função good não possui parametros
+    {
+        // Enquanto for possivel extrair caracteres do arquivo...
+        cout << c; // mostrando o primeiro caractere
+        c = in.get(); // pegando o proximo caractere
+        texto.push_back(c); // Inserindo o caractere na minha string caso queira usar nov
+    }
 
     return 0;
 }
