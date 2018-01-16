ce100a0f9bc5b01c7fe16ef6499ff25e692a5faf
Emanuelle Costa <eme@vonlou.dev>
2018-01-16 16:14:14 -0300

    Fstream

--- arq5.cpp
+++ arq5.cpp
@@ -10,8 +10,8 @@ int main(int argc,char *argv[])
     while(true) // enquanto for verdade
     {
         in >> c; // operador de deslocamento a direita
-        if(in.fail())
-            break;
+        if(in.fail()) // Função pra testar se tem algum caractere pra ser lido.
+            break; // Se não tiver nenhum caractere pra ser lido,vai dar um um break.
         cout << c;
     }
     return 0;
