b902f738480e84ecdf3775d137d2a31eba7b3a0b
emevonlou <emanuellebc@gmail.com>
2018-01-16 18:30:05 -0300

    Fstream

--- arq5.cpp
+++ arq5.cpp
@@ -3,16 +3,16 @@
 #include <fstream>
 using namespace std;
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
     ifstream in("teste.txt");
     char c;
 
-    while(true) // Enquanto for verdade, faça:
+    while (true) // Enquanto for verdade, faça:
     {
-        in >> c; // Operador de deslocamento a direita.
-        if(in.fail()) // Função pra testar se tem algum caractere pra ser lido.
-            break; // Se não tiver nenhum caractere pra ser lido,vai dar um um break.
+        in >> c;       // Operador de deslocamento a direita.
+        if (in.fail()) // Função pra testar se tem algum caractere pra ser lido.
+            break;     // Se não tiver nenhum caractere pra ser lido,vai dar um um break.
         cout << c;
     }
     return 0;
