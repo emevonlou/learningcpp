08dcacdaca7b713d78dcf23bee9f9c0288225c66
Emanuelle Costa <eme@vonlou.dev>
2018-01-16 15:46:18 -0300

    Fstream!

--- arq3.cpp
+++ arq3.cpp
@@ -1,10 +1,20 @@
 // 132:
+// Abrir um arquivo para a entrada
+// Ler o conteudo desse arquivo
 #include <iostream>
-#include <fstream>
+#include <fstream> // Abrir um arquivo
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+    ifstream in("teste.txt");
     
+    string texto; 
+    // classe string redimenciona automaticamente para a quant de dados necessaria
+    char c = in.get(); // metodo 'get' retorna o proximo caractere
+    texto.push_back(c); // Insere o caractere na string
+
+    cout << "\nMostrando cada caracere:\n" << endl;
+
     return 0;
 }
