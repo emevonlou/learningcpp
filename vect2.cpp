e315cf0fb6c6da24a008e72a7e5b5fcb03dae32a
emevonlou <emanuellebc@gmail.com>
2018-01-18 13:41:41 -0300

    classe Vector

---
+++ vect2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    vector<int> v(3);  // obtendo 3 como tamanho inicial do vetor.
+
+  return 0;
+}
