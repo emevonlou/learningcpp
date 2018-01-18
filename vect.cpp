e4c6c28d005ec84b72fb8a61e3eec9081b2ca177
Emanuelle Costa <eme@vonlou.dev>
2018-01-18 13:31:58 -0300

    excecoes(Try, catch, trow)

---
+++ vect.cpp
@@ -0,0 +1,17 @@
+// 136:
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int atgc, char *argv[])
+{
+    vector<int> v;
+
+    v.push_back(20); // A funcao 'push_back' Insere elementos no vetor.
+    v.push_back(30);
+    v.push_back(40);
+
+
+
+    return 0;
+}
