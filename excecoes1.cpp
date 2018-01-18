b308e26684ef5b0f45c462af46298b27819a79cf
Emanuelle Costa <eme@vonlou.dev>
2018-01-18 11:28:30 -0300

    excecoes(Try, catch, trow)

---
+++ excecoes1.cpp
@@ -0,0 +1,15 @@
+/*
+     Palavras reservadas 'try', 'catch','trhow'.
+     *Try: Tentar executar um bloco de codigo;
+     *Catch: Pegar algo para tratar;
+     *Trow: Lança uma excecao(erro) que a funcao pode pear através do 'catch'.
+*/
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    
+
+    return 0;
+}
