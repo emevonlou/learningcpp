f0c19b02557550d52fabbfb8e81051a5e01cb7a2
Emanuelle Costa <eme@vonlou.dev>
2018-01-18 13:46:01 -0300

    classe Vector

--- vect2.cpp
+++ vect2.cpp
@@ -6,6 +6,14 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     vector<int> v(3);  // obtendo 3 como tamanho inicial do vetor.
+    v[0] = 10;
+    v[1] = 20;
+    v[2] = 30;
+
+    for(unsigned int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << endl;
+    }
 
   return 0;
 }
