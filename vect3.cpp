efb355bdb740dcc0e6b38ba97484699f23622c33
emevonlou <emanuellebc@gmail.com>
2018-01-18 13:56:25 -0300

    classe Vector

--- vect3.cpp
+++ vect3.cpp
@@ -5,12 +5,15 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    
     vector<int> v(3);  // obtendo 3 como tamanho inicial do vetor.
     v[0] = 10;
     v[1] = 20;
     v[2] = 30;
 
-    vector<int>::iterator it;
+    vector<int>::iterator it = v.begin();
+    // Begin retorna o iterator que aponta para o primeiro elemento.
+    cout << *it << endl;
 
   return 0;
 }
