b492fdf160ecc9a5d3b013e437e24d278d9d391a
emevonlou <emanuellebc@gmail.com>
2018-01-18 13:47:29 -0300

    classe Vector

--- vect2.cpp
+++ vect2.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     v[2] = 30;
 
     for(unsigned int i = 0; i < v.size(); i++)
-    {
+    { // Para comparações entre expressoes inteiras com sinal e sem sinal use 'unsigned'.
         cout << v[i] << endl;
     }
 
