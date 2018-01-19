d726b8d3f7e4cb83b60895f5041289deaef50d1a
Emanuelle Costa <eme@vonlou.dev>
2018-01-19 14:19:02 -0300

    reverse iterator

--- vect5.cpp
+++ vect5.cpp
@@ -7,17 +7,17 @@ int main(int argc, char *argv[])
 {
 
     vector<int> v(3); // Obtendo 3 como tamanho inicial do vetor.
-    
+
     vector<int>::reverse_iterator rit;
     int i = 0; // Utilizada para atribuir valores para o vetor.
-
-    for(rit = v.rbegin(); rit != v.rend(); rit++) // For do iterador.
-    { // rbegin(),rend() -> retorna o iterador reverso que aponta para o ultimo elemento do vetor.
+    // começamdo do zero.
+    for (rit = v.rbegin(); rit != v.rend(); rit++) // For do iterador.
+    {                                              // rbegin(),rend() -> retorna o iterador reverso que aponta para o ultimo elemento do vetor.
         *rit = ++i;
     }
 
     vector<int>::iterator it;
-    for(it = v.begin(); it != v.end(); it++)
+    for (it = v.begin(); it != v.end(); it++)
     {
         cout << *it << endl;
     }
