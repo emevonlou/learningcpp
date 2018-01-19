11e3b543a14a3ba6a9db1b38fdda1ff9a8abc8db
Emanuelle Costa <eme@vonlou.dev>
2018-01-19 14:15:15 -0300

    reverse iterator

--- vect5.cpp
+++ vect5.cpp
@@ -14,9 +14,11 @@ int main(int argc, char *argv[])
     vector<int>::reverse_iterator rit;
     int i = 0; // Utilizada para atribuir valores para o vetor.
 
-    for(rit = v.rbegin(); rit != v.rend(); rit++) // 
-    {
-
+    for(rit = v.rbegin(); rit != v.rend(); rit++) // For do iterador.
+    { // rbegin(),rend() -> retorna o iterador reverso que aponta para o ultimo elemento do vetor.
+        *rit = ++i;
     }
+
+    vector<int>::iterator it;
     return 0;
 }
