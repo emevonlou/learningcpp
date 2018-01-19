603eadde4c035de761655b755493c7c92277558a
Emanuelle Costa <eme@vonlou.dev>
2018-01-19 14:09:40 -0300

    reverse iterator

---
+++ vect5.cpp
@@ -0,0 +1,17 @@
+// 140:
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+    vector<int> v(3); // Obtendo 3 como tamanho inicial do vetor.
+    v[0] = 10;
+    v[1] = 20;
+    v[2] = 30;
+    
+    vector<int>::reverse_iterator rit;
+    int i = 0; // 
+    return 0;
+}
