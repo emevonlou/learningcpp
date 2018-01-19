0b388616fab06c4ba8c38f681718e060b9f36972
emevonlou <emanuellebc@gmail.com>
2018-01-19 14:17:46 -0300

    reverse iterator

--- vect5.cpp
+++ vect5.cpp
@@ -7,9 +7,6 @@ int main(int argc, char *argv[])
 {
 
     vector<int> v(3); // Obtendo 3 como tamanho inicial do vetor.
-    v[0] = 10;
-    v[1] = 20;
-    v[2] = 30;
     
     vector<int>::reverse_iterator rit;
     int i = 0; // Utilizada para atribuir valores para o vetor.
