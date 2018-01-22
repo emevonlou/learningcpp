46ce3831b477ca6dad0de9f552bd8a41540688cc
emevonlou <emanuellebc@gmail.com>
2018-01-22 16:33:13 -0300

    vector

--- vect11.cpp
+++ vect11.cpp
@@ -8,10 +8,10 @@ int main(int argc, char *argv[])
     vector<int> b(3, 10);
 
     a.swap(b);
-    for(unsigned int i = 0; i < a.size(); i++)
-         cout << a[i] << endl;
-    for(unsigned int j = 0; j < a.size(); j++)
-         cout << b[j] << endl;
+    for (unsigned int i = 0; i < a.size(); i++)
+        cout << a[i] << endl;
+    for (unsigned int j = 0; j < a.size(); j++)
+        cout << b[j] << endl;
 
     return 0;
 }
