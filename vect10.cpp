d60235e97f9ce65a51b91468dfb8165e7ab2018a
emevonlou <emanuellebc@gmail.com>
2018-01-22 16:21:06 -0300

    vector

--- vect10.cpp
+++ vect10.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     vector<int>::iterator it = v.begin();
     v.insert(it, 40);
     v.insert(it + 2, 60);
-    v.erase(v.begin()+ 1);  // Apaga o segundo elemento do vetor.
+    v.erase(v.begin() + 1); // Apaga o segundo elemento do vetor.
 
     for (unsigned int i = 0; i < v.size(); i++)
         cout << v[i] << endl;
