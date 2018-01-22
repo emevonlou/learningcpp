d8734551361c739cddceac61ec6edcbfc854496e
Emanuelle Costa <eme@vonlou.dev>
2018-01-22 16:16:41 -0300

    vector

--- vect10.cpp
+++ vect10.cpp
@@ -13,6 +13,7 @@ int main(int argc, char *argv[])
 
     vector<int>::iterator it = v.begin();
     v.insert(it, 40);
+    v.insert(it + 2, 60);
 
     for (unsigned int i = 0; i < v.size(); i++)
         cout << v[i] << endl;
