c7181ee7c6c7b8f16be5bff75597340be3d00bfc
Emanuelle Costa <eme@vonlou.dev>
2018-01-22 16:13:51 -0300

    vector

---
+++ vect10.cpp
@@ -0,0 +1,21 @@
+// Função insert()
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    vector<int> v(3);
+
+    v[0] = 10;
+    v[1] = 20;
+    v[2] = 30;
+
+    vector<int>::iterator it = v.begin();
+    v.insert(it, 40);
+
+    for(unsigned int i = 0; i < v.size(); i++)
+        cout << v[i] << endl;
+
+    return 0;
+}
