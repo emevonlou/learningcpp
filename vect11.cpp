26202d0b620a5e8d3191dcdf346ab1416eef5f0f
emevonlou <emanuellebc@gmail.com>
2018-01-22 16:35:25 -0300

    vector

--- vect11.cpp
+++ vect11.cpp
@@ -7,7 +7,8 @@ int main(int argc, char *argv[])
     vector<int> a(2, 20);
     vector<int> b(3, 10);
 
-    a.swap(b);
+    a.swap(b); // Funcao swap() para a troca de vetores.
+
     for (unsigned int i = 0; i < a.size(); i++)
         cout << a[i] << endl;
     for (unsigned int j = 0; j < b.size(); j++)
