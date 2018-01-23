bed3b2e4054291f856b34d313378c831b2945813
emevonlou <emanuellebc@gmail.com>
2018-01-23 10:12:57 -0300

    vector

--- vect2.cpp
+++ vect2.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    vector<int> v(3);  // obtendo 3 como tamanho inicial do vetor.
+    vector<int> v(3); // obtendo 3 como tamanho inicial do vetor.
     v[0] = 10;
     v[1] = 20;
     v[2] = 30;
 
-    for(unsigned int i = 0; i < v.size(); i++)
+    for (unsigned int i = 0; i < v.size(); i++)
     { // Para comparações entre expressoes inteiras com sinal e sem sinal use 'unsigned'.
         cout << v[i] << endl;
     }
 
-  return 0;
+    return 0;
 }
