411c539c12f5bc6835d975105b85777871d1eca7
emevonlou <emanuellebc@gmail.com>
2018-01-23 13:40:22 -0300

    randtm

--- randtm.cpp
+++ randtm.cpp
@@ -1,25 +1,28 @@
 // rand() <3
-#include <stdio.h>      /* printf, scanf, puts, NULL */
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
+#include <stdio.h>  /* printf, scanf, puts, NULL */
+#include <stdlib.h> /* srand, rand */
+#include <time.h>   /* time */
 
-int main ()
+int main()
 {
-  int iSecret, iGuess;
+    int iSecret, iGuess;
 
-  /* initialize random seed: */
-  srand (time(NULL));
+    /* initialize random seed: */
+    srand(time(NULL));
 
-  /* generate secret number between 1 and 10: */
-  iSecret = rand() % 10 + 1;
+    /* generate secret number between 1 and 100: */
+    iSecret = rand() % 100 + 1;
 
-  do {
-    printf ("Guess the number (1 to 10): ");
-    scanf ("%d",&iGuess);
-    if (iSecret<iGuess) puts ("The secret number is lower");
-    else if (iSecret>iGuess) puts ("The secret number is higher");
-  } while (iSecret!=iGuess);
+    do
+    {
+        printf("Guess the number (1 to 100): ");
+        scanf("%d", &iGuess);
+        if (iSecret < iGuess)
+            puts("The secret number is lower");
+        else if (iSecret > iGuess)
+            puts("The secret number is higher");
+    } while (iSecret != iGuess);
 
-  puts ("Congratulations!");
-  return 0;
+    puts("Congratulations!");
+    return 0;
 }
