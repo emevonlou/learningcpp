6d46c821e2d53a5ef2bf80356b4adbeda58762e0
emevonlou <emanuellebc@gmail.com>
2018-01-24 14:53:48 -0300

    list

--- list1.cpp
+++ list1.cpp
@@ -12,11 +12,11 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    list<int> l1; // Lista de inteiros vazia
-    list<int> l2(3,10); // Trẽs inteiros com o valor 10 
+    list<int> l1; // Lista de inteiros vazia.
+    list<int> l2(3,10); // Trẽs inteiros com o valor 10. 
     list<int>::iterator it;
 
-    // Inserindo elementos em l1 - push_back e push_front
+    // Inserindo elementos em l1 - push_back e push_front.
     l1.push_back(10); // l1:10
     l1.push_front(20); // l1: 20, 10
     l1.push_back(30); // l1: 20, 10, 30 
