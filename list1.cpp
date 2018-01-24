384890682e0481fdced3640b8735fd41d59ed52c
emevonlou <emanuellebc@gmail.com>
2018-01-24 14:36:20 -0300

    list

--- list1.cpp
+++ list1.cpp
@@ -3,6 +3,7 @@
     * A list não prealoca espaços na memoria para elementos futuros.
     * Cada elemento requer espaco extra para o no que detem o elemnto.
       (incluindo ponteiros que apontam p elemento/seguinte e anterior)
+    * Você nao pode acessar aleatoriamente os elementos.
 */
 #include <iostream>
 using namespace std;
