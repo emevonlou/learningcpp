27217020f49187c46e091ca1a6bf58d12aa885c6
Emanuelle Costa <eme@vonlou.dev>
2018-01-24 14:34:27 -0300

    update

--- list1.cpp
+++ list1.cpp
@@ -3,7 +3,7 @@
     * A list não prealoca espaços na memoria para elementos futuros.
     * Cada elemento requer espaco extra para o no que detem o elemnto.
       (incluindo ponteiros que apontam p elemento/seguinte e anterior)
-*/ 
+*/
 #include <iostream>
 using namespace std;
 
