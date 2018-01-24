a0a6779741aa1c5bfc7d437545cfda71399c545a
Emanuelle Costa <eme@vonlou.dev>
2018-01-24 14:52:44 -0300

    list

--- list1.cpp
+++ list1.cpp
@@ -16,10 +16,10 @@ int main(int argc, char *argv[])
     list<int> l2(3,10); // Trẽs inteiros com o valor 10 
     list<int>::iterator it;
 
-    //
-    l1.push_back(10);
-    l1.push_front(20);
-    l1.push_back(30);
+    // Inserindo elementos em l1 - push_back e push_front
+    l1.push_back(10); // l1:10
+    l1.push_front(20); // l1: 20, 10
+    l1.push_back(30); // l1: 20, 10, 30 
 
     cout << "Mostrando os elementos de l1: " << endl;
 
