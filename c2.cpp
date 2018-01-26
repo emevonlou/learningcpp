4c73bcd690f4a13853d39d8a2be1c2a875799d04
emevonlou <emanuellebc@gmail.com>
2018-01-26 17:43:03 -0300

    Adicionando comentarios importantes

--- c2.cpp
+++ c2.cpp
@@ -6,8 +6,14 @@ int main(int argc, char *argv[])
 {
     // Variaveis constantes devem ser inicializadas quando criadas.
     const double PI = 3.1415;
-    
+
     cout << PI << endl;
+
+    /*
+        A última instrução no programa é a declaração de retorno. A linha retorna 0;
+        termina a função principal () e faz com que ele retorne o valor 0 ao processo de chamada. 
+        Um valor não-zero (geralmente de 1) sinaliza uma terminação anormal.
+    */
 
     return 0;
 }
