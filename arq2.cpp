d5143cb7f56af68efc58e1c713f20383ebfdec4b
emevonlou <emanuellebc@gmail.com>
2018-01-26 17:26:48 -0300

    Adicionando comentarios importantes

--- arq2.cpp
+++ arq2.cpp
@@ -1,24 +1,24 @@
 // 131:
 #include <iostream> // Serve pra executar o fluxo de 'io'(Entrada e saida).
 using namespace std;
-
+/*
+    O ponto de entrada de cada programa C ++ é main ()
+    independente do que o programa faz.
+*/
 int main(int argc, char *argv[])
 {
-    /*
-        O ponto de entrada de cada programa C ++ é main ()
-        independente do que o programa faz.
-    */
     int num;
 
-    cout << "Digite um numero: " << endl;
-    cin >> num;
-
-    cout << "Voce digitou " << num << endl;
     /*
         Na maioria dos ambientes de programa,
         o destino de saída padrão padrão é a tela. Em C ++,
         cout é o objeto de fluxo usado para acessá-lo.
     */
+
+    cout << "Digite um numero: " << endl;
+    cin >> num;
+
+    cout << "Voce digitou " << num << endl;
 
     return 0;
 }
