08b7ebd04111508e58916a44d409951c44c74dbe
emevonlou <emanuellebc@gmail.com>
2018-01-26 17:03:45 -0300

    Adicionando comentarios importantes

--- arq2.cpp
+++ arq2.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    /*
+        O ponto de entrada de cada programa C ++ é main ()
+        independente do que o programa faz.
+    */
     int num;
 
     cout << "Digite um numero: " << endl;
