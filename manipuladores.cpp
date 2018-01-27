336f92067526c7ceef6fe4c292c2561f7ade4d4e
Emanuelle Costa <eme@vonlou.dev>
2018-01-27 19:12:45 -0300

    Update manipuladores

--- manipuladores.cpp
+++ manipuladores.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    //obj cin = console.input.
-    //obj cout = console.output.
+    // obj cin = console.input.
+    // obj cout = console.output.
 
     cout << "Estudando a entrada e saida de dados: " << endl;
     cout << hex << 19 + 40 << endl;
