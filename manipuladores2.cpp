0a357432532912cd92280ec01965efaa2a5ac096
emevonlou <emanuellebc@gmail.com>
2018-01-27 19:16:24 -0300

    Update manipuladores

--- manipuladores2.cpp
+++ manipuladores2.cpp
@@ -1,13 +1,13 @@
 // 77:
 #include <iostream> // Traz um conjunto de instruções chamados de manipuladores.
-#include <stdlib.h> // system("pause")
-#include <iomanip>  // setw()
+#include <stdlib.h> // system("pause").
+#include <iomanip>  // setw().
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    //obj cin
-    //obj cout
+    // obj cin
+    // obj cout
 
     cout << "Estudando a entrada e saida de dados: " << endl; //OBJETO DE SAÍDA(cout)
     cout << setw(10) << 1 << endl;                            // setw() Estabelece tamanho de um determinado campo.
