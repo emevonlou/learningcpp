980b4cfba3cd9fd85098dc352a79765e97b801f4
emevonlou <emanuellebc@gmail.com>
2018-01-26 22:04:57 -0300

    Update

--- charfunc2.cpp
+++ charfunc2.cpp
@@ -9,21 +9,37 @@ int main(int argc, char *argv[])
     char c = '2';
 
     if (isalpha(c))
+    {
         cout << "Alfabetico!" << endl;
+    }
     else
+    {
         cout << "Nao alfabetico." << endl;
+    }
     if (isdigit(c))
+    {
         cout << "Digito!" << endl;
+    }
     else
+    {
         cout << "Nao eh digito." << endl;
+    }
     if (isupper(c))
+    {
         cout << "Maiuscula!" << endl;
+    }
     else
+    {
         cout << "Minuscula." << endl;
+    }
     if (islower(c))
+    {
         cout << "Minuscula!" << endl;
+    }
     else
+    {
         cout << "Maiuscula." << endl;
+    }
 
     char nome[] = "Emanuelle";
     char nome2[] = "emanuelle";
@@ -33,9 +49,13 @@ int main(int argc, char *argv[])
     cout << "Tamanho: " << strlen(nome) << endl;
 
     if (strcmp(nome, nome2) == 0)
+    {
         cout << "Strings Iguais.\n";
+    }
     else
+    {
         cout << "Strings Diferentes." << endl;
+    }
 
     strcpy(em, nome);
     cout << "Copiando de uma string pra outra:" << em << endl;
@@ -45,7 +65,7 @@ int main(int argc, char *argv[])
 
     char my_char = 'a';
 
-    char min_c = tolower(my_char);
+    // char min_c = tolower(my_char);
 
     cout << "min_c: " << my_char << endl;
 
