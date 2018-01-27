eb94939263b7e495cac9a47939dd967aade2f1f4
emevonlou <emanuellebc@gmail.com>
2018-01-27 19:17:48 -0300

    Update manipuladores

--- manipuladores2.cpp
+++ manipuladores2.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     // obj cin.
     // obj cout.
 
-    cout << "Estudando a entrada e saida de dados: " << endl; //OBJETO DE SAÍDA(cout)
+    cout << "Estudando a entrada e saida de dados: " << endl; // OBJETO DE SAÍDA(cout).
     cout << setw(10) << 1 << endl;                            // setw() Estabelece tamanho de um determinado campo.
     cout << setw(10) << 2 << endl;
     cout << setw(10) << 3 << endl;
