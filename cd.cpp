095e15fe636194584d6b1e85d99fef9c14724be2
Emanuelle Costa <eme@vonlou.dev>
2018-01-26 21:56:14 -0300

    explicando \ n

--- cd.cpp
+++ cd.cpp
@@ -2,10 +2,7 @@
 #include <cstdlib>
 #include <string>
 #include <iostream>
-
 using namespace std;
-
-// O construtor é um método que chama o proprio nome da classe constroe, altera e personaliza o objeto.
 
 int main(int argc, char *argv[])
 {
