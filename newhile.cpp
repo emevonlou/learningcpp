748740b203f2384f63f4cebb2bcbab64fc4bbb48
Emanuelle Costa <eme@vonlou.dev>
2018-01-27 19:41:22 -0300

    Update

--- newhile.cpp
+++ newhile.cpp
@@ -6,10 +6,10 @@ int main(int argc, char *argv[])
 {
     int n = 0;
 
-    while (1) // Enquanto for verdade faça.
+    while (1) // Enquanto for verdade faça...
     {
         n++;
-        if (n % 2 == 0) // Se o numero for par faça.
+        if (n % 2 == 0) // Se o numero for par faça...
             continue;
         if (n > 100)
             break;
