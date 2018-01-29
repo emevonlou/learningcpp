305309d30b2c422f43101fd7cf0483a09ec2092c
Emanuelle Costa <eme@vonlou.dev>
2018-01-29 14:53:02 -0300

    Using clog

--- wtf.cpp
+++ wtf.cpp
@@ -11,6 +11,12 @@ int main(int argc, char *argv[])
         cout << "Life is too short for bad coffee!" << endl;
         n++;
     }
+    /*
+        Quando tiver que adicionar informações ao LOG do programa,
+        vamos utilizar o objeto Console LOG, que numa tradução significa, 
+        "Console de informações de estado e funcionamento do programa".
+    
+    */
 
     clog << "With 'clog' is Better! YaY" << endl;
 
