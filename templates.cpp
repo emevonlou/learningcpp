6905ebb1aa8554410aa8370b6749880d237ec3de
Emanuelle Costa <eme@vonlou.dev>
2018-02-01 13:05:13 -0300

    Templates!

--- templates.cpp
+++ templates.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 using namespace std;
 
-// Com a mesma funcao eu realizo duas operacoes
-// com tipos diferentes
+// Com a mesma funcao eu realizo duas operacoes.
+// Com tipos diferentes.
 template<typename T> T soma(T n1, T n2)
 {
     return n1 + n2;
