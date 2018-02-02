000df7b6be55efea5aa29f8361e33e146008febc
emevonlou <emanuellebc@gmail.com>
2018-02-02 15:21:18 -0300

    Lista encadeada!

---
+++ encad.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class No
+{
+private:
+    int v;
+    No* prox; // referencia para o proximo 'No'
+public:
+    No(int v)
+    {
+        this->v = v;
+        this->prox = NULL;
+    }
+
+    int obterValor()
+    {
+        return v;
+    }
+
+};
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
