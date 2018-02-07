ef36b01b82a1bd2a765ec0fbb97ccb0512fc891c
emevonlou <emanuellebc@gmail.com>
2018-02-07 19:09:36 -0300

    Struct aninhada

--- saninhada.cpp
+++ saninhada.cpp
@@ -6,13 +6,13 @@ typedef struct Curso
 {
     char nome[50];
     double preco;
-}t_curso;
+} t_curso;
 
-typedef struct Estudante  // struct aninhada.
+typedef struct Estudante // struct aninhada.
 {
     char nome[50];
     Curso curso;
-}t_Estudante;
+} t_Estudante;
 
 int main(int argc, char *argv[])
 {
