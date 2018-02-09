6a4e3c178a7d0b59e68d3cfb808602169c2714fa
Emanuelle Costa <eme@vonlou.dev>
2018-02-09 20:44:46 -0300

    Retorno por referência.

--- returnref.cpp
+++ returnref.cpp
@@ -8,6 +8,7 @@
 */
 #include <iostream>
 #include <string>
+#include <math.h>
 using namespace std;
 
 typedef struct Pessoa
@@ -20,7 +21,8 @@ typedef struct Pessoa
 
 double& calcular_imc(t_pessoa & pessoa)
 {
-    double result = pessoa.peso / (pessoa.altura * pessoa.altura);
+    // double result = pessoa.peso / (pessoa.altura * pessoa.altura);
+    double result = pessoa.peso / pow(pessoa.altura, 2);
     double& imc = result;
 
     return imc;
@@ -31,6 +33,6 @@ int main(int argc, char *argv[])
     t_pessoa pessoa = {"Emanuelle", 39, 45.44, 1.55};
 
     cout << "IMC: " << calcular_imc(pessoa) << endl;
-    
+
     return 0;
 }
