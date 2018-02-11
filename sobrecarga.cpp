9424b083a9b380e4feebc89fa35c4888f53d5c2d
emevonlou <emanuellebc@gmail.com>
2018-02-11 17:57:43 -0300

    Sobrecarga do operador índice

--- sobrecarga.cpp
+++ sobrecarga.cpp
@@ -27,9 +27,9 @@ class Vetor
 
         // Alocar espaço.
         vet = (int *)malloc(max * sizeof(int));
-        vet_pos = (int*)malloc(max * sizeof(int));
+        vet_pos = (int *)malloc(max * sizeof(int));
 
-        for(int i = 0; i < max; i++)
+        for (int i = 0; i < max; i++)
         {
             vet_pos[i] = 0;
         }
@@ -58,7 +58,7 @@ class Vetor
             cerr << "Erro: Acesso invalido ao vetor!" << endl;
             exit(1);
         }
-        else if(vet_pos[i] == 0)
+        else if (vet_pos[i] == 0)
         {
             cerr << "Erro: Nessa posicao nao existe elemento" << endl;
             exit(1);
