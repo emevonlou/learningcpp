ed823a891f3cf0c7fc1cd0ddb8e8f078dd53652d
emevonlou <emanuellebc@gmail.com>
2018-02-11 18:01:28 -0300

    Sobrecarga do operador índice

--- sobrecarga.cpp
+++ sobrecarga.cpp
@@ -70,9 +70,9 @@ class Vetor
     {
         int cont = 0;
 
-        for(int i = 0; i < max; i++)
+        for (int i = 0; i < max; i++)
         {
-            if(vet_pos[i] == 1)
+            if (vet_pos[i] == 1)
                 cont++;
         }
         return cont;
