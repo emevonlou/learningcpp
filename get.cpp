eb94aeff201a06dfd226a7f1dfb73744e79a2b66
emevonlou <emanuellebc@gmail.com>
2018-02-12 09:56:48 -0300

    permitindo mais beleza ao código

--- get.cpp
+++ get.cpp
@@ -1,3 +1,4 @@
+// 163:
 #include <iostream>
 #define MAX 100
 using namespace std;
@@ -8,7 +9,7 @@ int main(int argc, char *argv[])
 
     cout << "Digite o seu nome: ";
     // cin >> nome;
-    cin.get(nome,MAX);  
+    cin.get(nome, MAX);
     cout << "Oi" << nome << endl;
 
     return 0;
