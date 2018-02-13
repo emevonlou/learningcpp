94c32534ca537fbf8a6e19864382b5f1f6a848ea
Emanuelle Costa <eme@vonlou.dev>
2018-02-13 17:03:55 -0300

    Classe deque

---
+++ deque1.cpp
@@ -0,0 +1,14 @@
+/* 
+    deque:
+    Classe que já vem com estrutura implementada
+    com diversas funcoes pra utilizar otimizando o 
+    desenvolvimento.
+*/
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
