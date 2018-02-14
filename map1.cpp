f7e6fdb134dff6c02dd11a8d5b4fd1624915ce7d
Emanuelle Costa <eme@vonlou.dev>
2018-02-14 16:57:05 -0300

    Classe/map

--- map1.cpp
+++ map1.cpp
@@ -5,6 +5,14 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    
+    pair<int, string> pares[] =
+        {
+            make_pair(1, "Emanuelle"),
+            make_pair(1, "Emanuelle"),
+            make_pair(1, "Emanuelle")
+
+        };
+    map<int, string> mapa(pares, pares + 3);
+
     return 0;
 }
