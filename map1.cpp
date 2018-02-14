d4fbddb608c9167693943a271b9b41d060a5893e
Emanuelle Costa <eme@vonlou.dev>
2018-02-14 16:57:31 -0300

    Classe/map

--- map1.cpp
+++ map1.cpp
@@ -8,8 +8,8 @@ int main(int argc, char *argv[])
     pair<int, string> pares[] =
         {
             make_pair(1, "Emanuelle"),
-            make_pair(1, "Emanuelle"),
-            make_pair(1, "Emanuelle")
+            make_pair(1, "Joaquim"),
+            make_pair(1, "Joao")
 
         };
     map<int, string> mapa(pares, pares + 3);
