1d4231a0474712b87f83dc31712e5628b271e6c3
Emanuelle Costa <eme@vonlou.dev>
2018-02-16 19:12:36 -0300

    classe queue-Mostrando o ultimo elemento

--- queue1.cpp
+++ queue1.cpp
@@ -8,9 +8,14 @@ int main(int argc, char *argv[])
 {
     queue<int> fila;
 
+    // Inserindo na fila
     fila.push(10);
     fila.push(20);
     fila.push(30);
+    // fila: 10 20 30
+
+    // Mostrando o ultimo elemento.
+    cout << fila.back() << endl;
 
     return 0;
 }
