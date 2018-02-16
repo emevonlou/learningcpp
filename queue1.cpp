ab058f8f5b998b670cbae90ebcefc148040aa11b
Emanuelle Costa <eme@vonlou.dev>
2018-02-16 19:11:02 -0300

    classe queue- Introdução

---
+++ queue1.cpp
@@ -0,0 +1,16 @@
+// 167: queue->classe que implementa filas.
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+    queue<int> fila;
+
+    fila.push(10);
+    fila.push(20);
+    fila.push(30);
+
+    return 0;
+}
