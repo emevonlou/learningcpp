e6c149d936374081fc1a715ba3b0f27b65b28438
emevonlou <emanuellebc@gmail.com>
2018-02-17 19:50:17 -0300

    Adicionando uma struct dentro da classe.

--- qpriority2.cpp
+++ qpriority2.cpp
@@ -9,9 +9,18 @@ public:
     int idade;
 };
 
+struct CompIdade
+{
+    bool operator()(Pessoa const & p1, Pessoa const & p2)
+    {
+        return p1.idade < p2.idade;
+    }
+};
+
+
 int main(int argc, char *argv[])
 {
-    
+    priority_queue<Pessoa, vector<Pessoa>, CompIdade > pq;
 
     return 0;
 }
