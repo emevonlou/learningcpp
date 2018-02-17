a4f2db3ae4070733114ac2c08fea373dac78c4af
emevonlou <emanuellebc@gmail.com>
2018-02-17 19:46:26 -0300

    queue com classe!

---
+++ qpriority2.cpp
@@ -0,0 +1,17 @@
+// 169: queue com classe.
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Pessoa
+{
+public:
+    int idade;
+};
+
+int main(int argc, char *argv[])
+{
+    
+
+    return 0;
+}
