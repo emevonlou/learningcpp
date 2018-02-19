9525df2fcdb6a936828c06a1ac759f1e290c073e
emevonlou <emanuellebc@gmail.com>
2018-02-19 14:54:08 -0300

    Add biblioteca fstream e criando uma classe.

--- arquivos1.cpp
+++ arquivos1.cpp
@@ -1,8 +1,22 @@
 // Gravando objetos em Arquivos.
 #include <iostream>
+#include <fstream> // ofstream e ifstream(output e input)
 using namespace std;
+
+class pessoa
+{
+private:
+    string nome;
+    int idade;
+public:
+    string& getNome()
+    {
+        return nome;
+    }
+};
 
 int main(int argc, char *argv[])
 {
+
     return 0;
 }
