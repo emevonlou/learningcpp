7fea7a5238ae68fb8a15ffaa419216968e261398
emevonlou <emanuellebc@gmail.com>
2018-02-20 17:23:41 -0300

    Grafos-Conceitos.

--- grafos.cpp
+++ grafos.cpp
@@ -17,16 +17,16 @@ int grafo[5][5] =
         {0, 0, 1, 0, 1},
         {0, 0, 1, 1, 0}};
 
-bool tem_ligacao(int [v1],int [v2])
+bool tem_ligacao(int[v1], int[v2])
 {
-    if(grafo[v1][v2])
+    if (grafo[v1][v2])
         return true;
-            return false;
+    return false;
 }
 
 int main(int argc, char *argv[])
 {
 
-    cout << tem_ligacao(0,2) << endl;
+    cout << tem_ligacao(0, 2) << endl;
     return 0;
 }
