d308fff531461d23ee3ac0acd7ad9d615461cd56
emevonlou <emanuellebc@gmail.com>
2018-02-20 17:09:19 -0300

    Grafos!

---
+++ grafos.cpp
@@ -0,0 +1,15 @@
+/*
+    173:
+    Grafo:
+    É uma entidade composta de duas partes:
+    Vértices(nós) e Arestas(linhas).
+    Os nós são as entidades("bolinhas") que você quer modelar.
+    As arestas(Arcos) são as relações dessas entidades.
+*/
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
