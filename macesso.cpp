035c7b297be2f2c618b39e84161a943ff46a2486
Emanuelle Costa <eme@vonlou.dev>
2018-02-21 18:41:54 -0300

    Modificando o acesso.

--- macesso.cpp
+++ macesso.cpp
@@ -6,19 +6,25 @@ using namespace std;
 class Linguagem
 {
   protected:
-    // protected: A classe fica responsável pela própria situação.
+    // protected: A classe fica responsável pela própria situação interna.
     // Os membros publicos formam a interface da classe.
     char nome[100];
 
-  public:
+  protected:
     void mostrarNome()
     {
         cout << "Nome: " << nome << endl;
     }
 
+  public:
+
     void setNome(const char *nome)
     {
         strcpy(this->nome, nome);
+    }
+    void mostrarNome2()
+    {
+        mostrarNome();
     }
 };
 
@@ -27,6 +33,6 @@ int main(int argc, char *argv[])
     Linguagem l;
 
     l.setNome("c++");
-    l.mostrarNome();
+    l.mostrarNome2();
     return 0;
 }
