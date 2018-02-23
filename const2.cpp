faee667f938986c352635bedd523c7171ab894a2
Emanuelle Costa <eme@vonlou.dev>
2018-02-23 15:00:10 -0300

    Construtores e destrutores

---
+++ const2.cpp
@@ -0,0 +1,18 @@
+// Construtores e destrutores.
+#include <iostream>
+using namespace std;
+
+class Pessoa
+{
+    public:
+        char nome[100];
+        int idade;
+};
+
+int main(int argc, char *argv[])
+{
+    Pessoa p = {"João", 30};
+
+
+    return 0;
+}
