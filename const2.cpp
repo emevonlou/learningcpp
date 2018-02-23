22c5e70914e9f3ff55d1be4fa95fd13902209e52
Emanuelle Costa <eme@vonlou.dev>
2018-02-23 15:10:00 -0300

    criando e inicializando o construtor para acessar os dados protegidos.

--- const2.cpp
+++ const2.cpp
@@ -1,20 +1,36 @@
 // Construtores e destrutores.
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 class Ficha
 {
-    public:
+    protected:
         char nome[100];
         int idade;
+    public:
+    // Inicializando o construtor para acessar os dados protegidos.
+        Ficha(const char* nome,int idade)  
+        {
+            strcpy(this->nome, nome);
+            this->idade = idade;
+        }
+        char* getNome()
+        {
+            return nome;
+        }
+        int getIdade()
+        {
+            return idade;
+        }
 };
 
 int main(int argc, char *argv[])
 {
-    Ficha p = {"Emanuelle", 38};
+    Ficha p("Emanuelle", 38);
 
-    cout << "Nome: " << p.nome << endl;
-    cout << "Idade: " << p.idade << endl;
+    cout << "Nome: " << p.getNome() << endl;
+    cout << "Idade: " << p.getIdade() << endl;
 
     return 0;
 }
