8ccf49874e43d137d6b570f442cc6876380dc32e
emevonlou <emanuellebc@gmail.com>
2018-02-25 12:49:17 -0300

    construindo cópia!

--- constcop.cpp
+++ constcop.cpp
@@ -1,4 +1,4 @@
-// 177: Cosntrutor de cópias(Também chamada de cópia superficial)!
+// 177: Cosntrutor de cópias!
 #include <iostream>
 #include <string.h>
 using namespace std;
@@ -12,10 +12,21 @@ class Estudante
     // Cosntrutor inicializando a idade com zero.
     Estudante(const char *nome)
     {
+        cout << "Construindo o objeto..." << endl;
         idade = 0;
         int tam = strlen(nome) + 1;
         this->nome = new char[tam]; // Alocando memória fora da pilha.
         strcpy(this->nome, nome);  // Copiando o nome para this->nome
+    }
+
+    // construtor de cópia para uma cópia não superficial
+    Estudante(const Estudante& e)
+    {
+        cout << "Construindo copia..." << endl;
+
+        int tam = strlen(e.nome)+ 1;
+        this->nome = new char[tam];
+        strcpy(this->nome, "copia de ");
     }
 };
 
