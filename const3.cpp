9797be61f0d423f33b0a43d745a80db2476e1f51
Emanuelle Costa <eme@vonlou.dev>
2018-02-25 12:24:15 -0300

    Adicionando outro construtor: preço.

--- const3.cpp
+++ const3.cpp
@@ -6,10 +6,12 @@ class Carro
 {
   public:
     int ano;
+    double preco;
     // Primeiro construtor: que não vai receber nada, apenas inicializar.
     Carro()
     {
         ano = 0; // inicializa pro ano 0
+        preco = 0.0; // Inicializando o preço
     }
 
     // Segundo construtor: Vai receber o ano
@@ -17,6 +19,12 @@ class Carro
     {
         // Atribui ao ano o ano deste objeto aqui.
         this->ano = ano;
+    }
+
+    Carro(int ano, double preco)
+    {
+        this->ano = ano;
+        this->preco = preco;
     }
 };
 
