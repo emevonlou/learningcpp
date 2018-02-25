f22df6bb57e59a4f48a2268d0249fb0579a0872c
Emanuelle Costa <eme@vonlou.dev>
2018-02-25 12:15:53 -0300

    Sobrecarregando Construtores

--- const3.cpp
+++ const3.cpp
@@ -2,8 +2,27 @@
 #include <iostream>
 using namespace std;
 
+class Carro
+{
+    public:
+        int ano;
+        // Primeiro construtor: que não vai receber nada, apenas inicializar.
+        Carro()
+        {
+            ano = 0; // inicializa pro ano 0
+        }
+
+        // Segundo construtor: Vai receber o ano
+        Carro(int ano)
+        {
+            // Atribui ao ano o ano deste objeto aqui.
+            this->ano = ano; 
+        }
+
+};
+
 int main(int argc, char *argv[])
 {
-    
+
     return 0;
 }
