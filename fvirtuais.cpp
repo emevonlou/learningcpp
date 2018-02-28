6d88a01a15f1fe8910d3c85b8fda3bfcf89bef48
Emanuelle Costa <eme@vonlou.dev>
2018-02-28 20:31:30 -0300

    Construindo duas classes

--- fvirtuais.cpp
+++ fvirtuais.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 class ClasseMae
 {
-    public:
-        void mostrarMensagem()
-        {
-            cout << "Ola, sou a classe mae!" << endl;
-        }
+  public:
+    void mostrarMensagem()
+    {
+        cout << "Ola, sou a classe mae!" << endl;
+    }
 };
 
-class ClasseFilha : public ClasseMae        // Herança
+class ClasseFilha : public ClasseMae // Herança
 {
-    public:
-        void mostrarMensagem()
-        {
-            cout << "Ola, sou a classe Filha!" << endl;
-        }
+  public:
+    void mostrarMensagem()
+    {
+        cout << "Ola, sou a classe Filha!" << endl;
+    }
 };
 
 int main(int argc, char *argv[])
