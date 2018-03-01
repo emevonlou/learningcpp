1e07c15976c601e975826eed549a591b58aebc7e
Emanuelle Costa <eme@vonlou.dev>
2018-03-01 13:30:46 -0300

    Adicionando um construtor.

--- pilha.cpp
+++ pilha.cpp
@@ -8,6 +8,11 @@ private:
     int * vet;   // Ponteiro que é meu vetor(vet).
     int max_tam;  // O máximo do tamanho da minha pilha.
     int topo;     // Variável que marca o topo da minha pilha.
+public:
+    Pilha()   // Construtor(Terá o mesmo nome da classe).
+    {
+
+    }
 };
 
 int main(int argc, char *argv[])
