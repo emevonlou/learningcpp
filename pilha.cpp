9fc7d13fb2f48bcff4f05952f0245093740b57a7
emevonlou <emanuellebc@gmail.com>
2018-03-01 13:34:03 -0300

    Momento identação!

--- pilha.cpp
+++ pilha.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 class Pilha
 {
-private:   
-    int * vet;   // Ponteiro que é meu vetor(vet).
-    int max_tam;  // O máximo do tamanho da minha pilha.
-    int topo;     // Variável que marca o topo da minha pilha.
-public:
-    Pilha()   // Construtor(Terá o mesmo nome da classe).
+  private:
+    int *vet;    // Ponteiro que é meu vetor(vet).
+    int max_tam; // O máximo do tamanho da minha pilha.
+    int topo;    // Variável que marca o topo da minha pilha.
+  public:
+    Pilha() // Construtor(Terá o mesmo nome da classe).
     {
-        vet = new int[100];   // Alocando espaço para 100 inteiros.
+        vet = new int[100]; // Alocando espaço para 100 inteiros.
         max_tam = 99;
-        topo = -1;   
+        topo = -1;
     }
 };
 
