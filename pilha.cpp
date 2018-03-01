f99f4dc6b7e6f5c0c017bb01f7a4ba7ec3157dbe
Emanuelle Costa <eme@vonlou.dev>
2018-03-01 13:29:41 -0300

    Variável que marca o topo da minha pilha.

--- pilha.cpp
+++ pilha.cpp
@@ -7,7 +7,7 @@ class Pilha
 private:   
     int * vet;   // Ponteiro que é meu vetor(vet).
     int max_tam;  // O máximo do tamanho da minha pilha.
-    
+    int topo;     // Variável que marca o topo da minha pilha.
 };
 
 int main(int argc, char *argv[])
