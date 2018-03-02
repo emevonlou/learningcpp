223df1de47d1321b94fbdfdba37a2e807da6adda
emevonlou <emanuellebc@gmail.com>
2018-03-02 17:40:46 -0300

    Primeiro exemplo-Inserndo string pra imprimir o nome.

--- string.cpp
+++ string.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-#include <string.h>
+#include <string>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    
+    string str = "EmanuelleVonLou";
+
+    cout << str << endl;
+
     return 0;
 }
