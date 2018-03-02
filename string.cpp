bd2494b0dd6a03decf7c373c02175a0db640e232
Emanuelle Costa <eme@vonlou.dev>
2018-03-02 17:49:22 -0300

    Funções para obter o primeiro e último caractere de uma string-front() e back().

--- string.cpp
+++ string.cpp
@@ -11,6 +11,11 @@ int main(int argc, char *argv[])
     cout << "Tamanho: " << str.length() << endl;
     // Passando uma posição para a função retornar o caractere que pode ser acessado atraves do numero.
     cout << str.at(1) << endl;
+    // Função Para obter o ultimo caractere de uma string
+    // Disponivel para o c++11
+    // cout << str.back() << endl;
+    // Função para obter o primeiro caractere de uma string
+    // cout << str.front() << endl;
 
     return 0;
 }
