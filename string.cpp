e82267e7260f5f0531dc6b66a22578b0fb55f4db
emevonlou <emanuellebc@gmail.com>
2018-03-02 17:41:58 -0300

    Verificando o tamanho da string.

--- string.cpp
+++ string.cpp
@@ -6,7 +6,8 @@ int main(int argc, char *argv[])
 {
     string str = "EmanuelleVonLou";
 
-    cout << str << endl;
+    cout << "String: " << str << endl;
+    cout << "Tamanho: " << str.size() << endl;
 
     return 0;
 }
