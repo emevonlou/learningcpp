b9c8c1fa1c1698b09801324d88a5bfbb012b3d26
emevonlou <emanuellebc@gmail.com>
2018-03-05 16:29:38 -0300

    Criando classe com odificador de acesso(private]) e dois atributos

---
+++ revisao2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Pessoa
+{
+// Modificador de acesso
+private:
+    // Dois atributos: Idade e um ponteiro.
+    int idade;
+    int *vet;
+};
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
