54faa50baac9dd58dd6f18c4072b972fd96d3445
emevonlou <emanuellebc@gmail.com>
2018-03-05 15:41:32 -0300

    Atribuindo variável inteira do tipo ponteiro, usando new para alocar espaço para dez inteiros, colocando 1 na primeira posição, colocando 2 na segunda posição

---
+++ revisao.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    // variável inteira do tipo ponteiro.
+    // Usando new para alocar espaço para dez inteiros.
+    int * p = new int[10];
+
+    // Colocando 1 na primeira posição.
+    *p = 1;
+    // Segunda posição: Colocando 2.
+    *(p + 1) = 2;
+    return 0;
+}
