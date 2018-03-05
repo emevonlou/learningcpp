6102a5d340d069af46e006f95a24d4ff1a2f5429
Emanuelle Costa <eme@vonlou.dev>
2018-03-05 15:44:21 -0300

    Liberando memória com delete[]

--- revisao.cpp
+++ revisao.cpp
@@ -5,15 +5,16 @@ int main(int argc, char *argv[])
 {
     // variável inteira do tipo ponteiro.
     // Usando new para alocar espaço para dez inteiros.
-    int * p = new int[10];
+    int *p = new int[10];
 
     // Colocando 1 na primeira posição.
     *p = 1;
     // Segunda posição: Colocando 2.
     *(p + 1) = 2;
 
-    cout << *p << endl << *(p + 1) << endl;
+    cout << *p << endl
+         << *(p + 1) << endl;
     // Liberando memória com delete []
-    delete [] p;
+    delete[] p;
     return 0;
 }
