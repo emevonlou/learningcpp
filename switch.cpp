259260e1582f3bd688af829d145b2b47dec89192
emevonlou <emanuellebc@gmail.com>
2018-03-23 08:59:46 -0300

    Momento Identação

--- switch.cpp
+++ switch.cpp
@@ -5,23 +5,22 @@ int main(int argc, char *argv[])
 {
     int n = 20;
 
-    switch(n)
+    switch (n)
     {
-        case 16:
-            cout << "Numero 16";
-            break;
-        case 17:
-            cout << "Numero 17";
-        case 20:
-            cout << "Numero 20";
-            break;
-        case 18:
-            cout << "Numero 18";
-            break;
-        default: 
-            cout << "Numero nao encontrado";
-            break;
-    
+    case 16:
+        cout << "Numero 16";
+        break;
+    case 17:
+        cout << "Numero 17";
+    case 20:
+        cout << "Numero 20";
+        break;
+    case 18:
+        cout << "Numero 18";
+        break;
+    default:
+        cout << "Numero nao encontrado";
+        break;
     }
     return 0;
 }
