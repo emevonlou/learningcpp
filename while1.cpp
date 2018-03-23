aded812751cbf209e26e170eb516f4a4a084af20
emevonlou <emanuellebc@gmail.com>
2018-03-23 13:31:24 -0300

    while!

--- while1.cpp
+++ while1.cpp
@@ -4,12 +4,14 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    string eme = "Emanuelle von lou";
+    char color[] = {0x1b, '[', '1', ';', '3', '5', 'm', 0};
+    cout << color;
+    string eme = "Emanuelle von lou <3";
     int n = 1;
 
     while(n == 1)
     {
-        cout << eme << " " << endl;
+        cout << eme << " ";
     }
     return 0;
 }
