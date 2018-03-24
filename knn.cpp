0b7cd425dacef33f36e146ef299423a8ac347093
emevonlou <emanuellebc@gmail.com>
2018-03-24 20:41:46 -0300

    Algoritmo KNN

---
+++ knn.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <math.h> // Calcular a distância euclidiana
+#include <set>
+#include <map>
+using namespace std;
+
+class Individuo
+{
+private: 
+    string classe;
+    double a, b , c , d;
+
+public: 
+    // Construtor
+    Individuo(double a, double b, double c, double d, string classe)
+    {
+        this->a = a;
+        this->b = b;
+        this->c = c;
+        this->d = c;
+        this->classe = classe;
+    }
+};
+
+// Calculando a distância euclidiana
+//double distEuclidiana()
+
+int main(int argc, char *argv[])
+{
+
+
+    return 0;
+}
