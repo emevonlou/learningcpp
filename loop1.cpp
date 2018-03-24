01fd2da5e01fdc28b8bc052d4a71c1ac0fec186c
Emanuelle Costa <eme@vonlou.dev>
2018-03-24 15:36:29 -0300

    Loop for love

--- loop1.cpp
+++ loop1.cpp
@@ -7,12 +7,26 @@ int main(int argc, char *argv[])
     char eme[] = {0x1b, '[', '1', ';', '3', '6', 'm', 0};
     cout << eme;
 
+    string nome;
     string m = "Eu te amo Joaquim";
     int i;
+    int j;
 
     for(i = 1; i <= 100; i++)
     {
         cout << m << " ";
+    }
+
+    char emes[] = {0x1b, '[', '1', ';', '3', '2', 'm', 0};
+    cout << emes;
+
+    cout << endl;
+    cout << "Digite um nome: " << endl;
+    cin >> nome;
+
+    for(j= 1; j <= 100; j++)
+    {
+        cout << "Parabens " << nome << " ";
     }
 
     return 0;
